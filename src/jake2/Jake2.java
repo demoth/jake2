a873cd3f8c4ff550de7aa8f784e11ee053dc21ac
Holger Zickner <hoz@bytonic.de>
2003-11-16 13:33:31 +0000

    *** empty log message ***

---
+++ src/jake2/Jake2.java
@@ -0,0 +1,20 @@
+/*
+ * Jake2.java
+ * Copyright (C) 2003
+ * 
+ * $Id: Jake2.java,v 1.1 2003-11-16 13:33:31 hoz Exp $
+ */
+package jake2;
+
+/**
+ * <code>Jake2</code>
+ * TODO There is still a lot of work to do.
+ */
+public class Jake2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+}
---
+++ src/test/Hello.java
@@ -0,0 +1,17 @@
+/*
+ * Hello.java
+ * Copyright (C) 2003
+ * 
+ * $Id$
+ */
+package test;
+
+/**
+ * <code>Hello</code>
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("F*** You!");
+	}
+}
