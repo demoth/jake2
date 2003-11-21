be35eadfe055ed9d27099a97a8aa1a36a591a600
Carsten Weisse <cwei@bytonic.de>
2003-11-21 23:28:53 +0000

    interface des renderers steht (aber noch keine Impl)

--- src/jake2/render/JoglRenderer.java
+++ src/jake2/render/JoglRenderer.java
@@ -2,9 +2,9 @@
  * JoglRenderer.java
  * Copyright (C) 2003
  *
- * $Id: JoglRenderer.java,v 1.1 2003-11-21 14:01:44 cwei Exp $
- */ 
- /*
+ * $Id: JoglRenderer.java,v 1.2 2003-11-21 23:28:53 cwei Exp $
+ */
+/*
 Copyright (C) 1997-2001 Id Software, Inc.
 
 This program is free software; you can redistribute it and/or
@@ -25,6 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 package jake2.render;
 
+import jake2.client.refdef_t;
 
 /**
  * JoglRenderer
@@ -32,16 +33,185 @@ package jake2.render;
  * @author cwei
  */
 final class JoglRenderer extends Renderer {
-	
+
 	JoglRenderer() {
 	}
 
 	/* 
-	 * @see jake2.ref_gl.Renderer#Init()
+	 * @see jake2.render.Renderer#Init()
 	 */
-	public void Init() {
-		// TODO Renderer Init()
-		
+	public boolean Init() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#Shutdown()
+	 */
+	public void Shutdown() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#BeginRegistration(java.lang.String)
+	 */
+	public void BeginRegistration(String map) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#RegisterModel(java.lang.String)
+	 */
+	public model_t RegisterModel(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#RegisterSkin(java.lang.String)
+	 */
+	public image_t RegisterSkin(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#RegisterPic(java.lang.String)
+	 */
+	public image_t RegisterPic(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#SetSky(java.lang.String, float, float[])
+	 */
+	public void SetSky(String name, float rotate, float[] axis) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#EndRegistration()
+	 */
+	public void EndRegistration() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#RenderFrame(jake2.client.refdef_t)
+	 */
+	public void RenderFrame(refdef_t fd) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#DrawGetPicSize(int[], int[], java.lang.String)
+	 */
+	public void DrawGetPicSize(int[] w, int[] h, String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#DrawPic(int, int, java.lang.String)
+	 */
+	public void DrawPic(int x, int y, String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#DrawStretchPic(int, int, int, int, java.lang.String)
+	 */
+	public void DrawStretchPic(int x, int y, int w, int h, String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#DrawChar(int, int, int)
+	 */
+	public void DrawChar(int x, int y, int c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#DrawTileClear(int, int, int, int, java.lang.String)
+	 */
+	public void DrawTileClear(int x, int y, int w, int h, String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#DrawFill(int, int, int, int, int)
+	 */
+	public void DrawFill(int x, int y, int w, int h, int c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#DrawFadeScreen()
+	 */
+	public void DrawFadeScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#DrawStretchRaw(int, int, int, int, int, int, byte[])
+	 */
+	public void DrawStretchRaw(
+		int x,
+		int y,
+		int w,
+		int h,
+		int cols,
+		int rows,
+		byte[] data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#CinematicSetPalette(byte[])
+	 */
+	public void CinematicSetPalette(byte[] palette) {
+		assert(
+			palette == null || palette.length == 768) : "cinematic palette bug";
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#BeginFrame(float)
+	 */
+	public void BeginFrame(float camera_separation) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#EndFrame()
+	 */
+	public void EndFrame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* 
+	 * @see jake2.render.Renderer#AppActivate(boolean)
+	 */
+	public void AppActivate(boolean activate) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
--- src/jake2/render/Renderer.java
+++ src/jake2/render/Renderer.java
@@ -2,9 +2,9 @@
  * Renderer.java
  * Copyright (C) 2003
  *
- * $Id: Renderer.java,v 1.1 2003-11-21 14:01:44 cwei Exp $
- */ 
- /*
+ * $Id: Renderer.java,v 1.2 2003-11-21 23:28:53 cwei Exp $
+ */
+/*
 Copyright (C) 1997-2001 Id Software, Inc.
 
 This program is free software; you can redistribute it and/or
@@ -25,6 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 package jake2.render;
 
+import jake2.client.refdef_t;
 
 /**
  * Renderer
@@ -32,9 +33,9 @@ package jake2.render;
  * @author cwei
  */
 public abstract class Renderer {
-	
+
 	private static Renderer impl = null;
-	
+
 	Renderer() {
 	}
 
@@ -49,13 +50,74 @@ public abstract class Renderer {
 		return impl;
 	}
 
-// ============================================================================
-// public interface for Renderer implementations
-// ============================================================================
- 	/**
- 	 * Initialize the Renderer.
- 	 */
-	public abstract void Init();
-	
-	
+	// ============================================================================
+	// public interface for Renderer implementations
+	//
+	// ref.h, refexport_t
+	// ============================================================================
+	//
+	// these are the functions exported by the refresh module
+	//
+	// called when the library is loaded
+	public abstract boolean Init();
+
+	// called before the library is unloaded
+	public abstract void Shutdown();
+
+	// All data that will be used in a level should be
+	// registered before rendering any frames to prevent disk hits,
+	// but they can still be registered at a later time
+	// if necessary.
+	//
+	// EndRegistration will free any remaining data that wasn't registered.
+	// Any model_s or skin_s pointers from before the BeginRegistration
+	// are no longer valid after EndRegistration.
+	//
+	// Skins and images need to be differentiated, because skins
+	// are flood filled to eliminate mip map edge errors, and pics have
+	// an implicit "pics/" prepended to the name. (a pic name that starts with a
+	// slash will not use the "pics/" prefix or the ".pcx" postfix)
+	public abstract void BeginRegistration(String map);
+	public abstract model_t RegisterModel(String name);
+	public abstract image_t RegisterSkin(String name);
+	public abstract image_t RegisterPic(String name);
+	public abstract void SetSky(String name, float rotate, /* vec3_t */
+	float[] axis);
+	public abstract void EndRegistration();
+
+	public abstract void RenderFrame(refdef_t fd);
+
+	public abstract void DrawGetPicSize(int[] w, int[] h, String name);
+	// will return 0 0 if not found
+	public abstract void DrawPic(int x, int y, String name);
+	public abstract void DrawStretchPic(
+		int x,
+		int y,
+		int w,
+		int h,
+		String name);
+	public abstract void DrawChar(int x, int y, int c);
+	public abstract void DrawTileClear(int x, int y, int w, int h, String name);
+	public abstract void DrawFill(int x, int y, int w, int h, int c);
+	public abstract void DrawFadeScreen();
+
+	// Draw images for cinematic rendering (which can have a different palette). Note that calls
+	public abstract void DrawStretchRaw(
+		int x,
+		int y,
+		int w,
+		int h,
+		int cols,
+		int rows,
+		byte[] data);
+
+	/*
+	** video mode and refresh state management entry points
+	*/
+	/* 256 r,g,b values;	null = game palette */
+	public abstract void CinematicSetPalette(final byte[] palette);
+	public abstract void BeginFrame(float camera_separation);
+	public abstract void EndFrame();
+
+	public abstract void AppActivate(boolean activate);
 }
