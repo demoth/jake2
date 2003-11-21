14e1505e8aa83d640f44d7da943f6e56a697b40c
Carsten Weisse <cwei@bytonic.de>
2003-11-21 14:01:44 +0000

    default renderer is jogl

---
+++ src/jake2/render/JoglRenderer.java
@@ -0,0 +1,47 @@
+/*
+ * JoglRenderer.java
+ * Copyright (C) 2003
+ *
+ * $Id: JoglRenderer.java,v 1.1 2003-11-21 14:01:44 cwei Exp $
+ */ 
+ /*
+Copyright (C) 1997-2001 Id Software, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+package jake2.render;
+
+
+/**
+ * JoglRenderer
+ * 
+ * @author cwei
+ */
+final class JoglRenderer extends Renderer {
+	
+	JoglRenderer() {
+	}
+
+	/* 
+	 * @see jake2.ref_gl.Renderer#Init()
+	 */
+	public void Init() {
+		// TODO Renderer Init()
+		
+	}
+
+}
