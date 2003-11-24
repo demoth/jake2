fe31693292ef87a8667bef057afc2afd2b755529
Carsten Weisse <cwei@bytonic.de>
2003-11-24 12:02:54 +0000

    refimport_t ist komplett

--- src/jake2/client/refimport_t.java
+++ src/jake2/client/refimport_t.java
@@ -19,31 +19,45 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 // Created on 20.11.2003 by RST.
+// modified by cwei
 
 package jake2.client;
 
+import java.awt.Dimension;
+
 import jake2.game.*;
+import jake2.qcommon.xcommand_t;
 
-public class refimport_t {
-	int		x, y, width, height;// in virtual screen coordinates
-	float		fov_x, fov_y;
-	float		vieworg[] ={0,0,0};
-	float		viewangles[]={0,0,0};
-	float		blend[]={0,0,0,0};			// rgba 0-1 full screen blend
-	float		time;				// time is uesed to auto animate
-	int		rdflags;			// RDF_UNDERWATER, etc
+public interface refimport_t {
+	// ref.h 
+	// these are the functions imported by the refresh module
+	//
+	void Sys_Error(int err_level, String str, Object[] vargs);
 
-	byte		areabits[];			// if not NULL, only areas with set bits will be drawn
+	void Cmd_AddCommand(String name, xcommand_t cmd);
+	void Cmd_RemoveCommand(String name);
+	int Cmd_Argc();
+	String Cmd_Argv(int i);
+	void Cmd_ExecuteText(int exec_when, String text);
 
-	lightstyle_t lightstyles;	// [MAX_LIGHTSTYLES]
+	void Con_Printf(int print_level, String str, Object[] vargs);
 
-	int		num_entities;
-	entity_t	entities[];
+	// files will be memory mapped read only
+	// the returned buffer may be part of a larger pak file,
+	// or a discrete file from anywhere in the quake search path
+	// a -1 return means the file does not exist
+	// NULL can be passed for buf to just determine existance
+	int FS_LoadFile(String name, byte[] buf);
+	void FS_FreeFile(byte[] buf);
+	// gamedir will be the current directory that generated
+	// files should be stored to, ie: "f:\quake\id1"
+	String FS_Gamedir();
 
-	int		num_dlights;
-	dlight_t	dlights;
+	cvar_t Cvar_Get(String name, String value, int flags);
+	cvar_t Cvar_Set(String name, String value);
+	void Cvar_SetValue(String name, float value);
 
-	int		num_particles;
-	particle_t	particles;
-
+	boolean Vid_GetModeInfo(Dimension dim /* int *w,  *h */, int mode);
+	void Vid_MenuInit();
+	void Vid_NewWindow(int width, int height);
 }
