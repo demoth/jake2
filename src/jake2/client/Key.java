1967f8a3f8119fb8bfb7947471cec5d61f34e15a
Holger Zickner <hoz@bytonic.de>
2004-01-07 13:07:45 +0000

    started with KBD crap

--- src/jake2/client/Key.java
+++ src/jake2/client/Key.java
@@ -2,7 +2,7 @@
  * Key.java
  * Copyright (C) 2003
  * 
- * $Id: Key.java,v 1.5 2004-01-02 14:08:20 hoz Exp $
+ * $Id: Key.java,v 1.6 2004-01-07 13:07:45 hoz Exp $
  */
  /*
 Copyright (C) 1997-2001 Id Software, Inc.
@@ -41,5 +41,175 @@ public final class Key {
 //	00058         key_lines[edit_line][1] = 0;    // clear any typing
 //	00059         key_linepos = 1;
 	}
-	
+
+	/**
+	 * Called by the system between frames for both key up and key down events.
+	 */
+	public static void Event(int key, boolean down, long time) {
+//	00715         char    *kb;
+//	00716         char    cmd[1024];
+//	00717 
+//	00718         // hack for modal presses
+//	00719         if (key_waiting == -1)
+//	00720         {
+//	00721                 if (down)
+//	00722                         key_waiting = key;
+//	00723                 return;
+//	00724         }
+//	00725 
+//	00726         // update auto-repeat status
+//	00727         if (down)
+//	00728         {
+//	00729                 key_repeats[key]++;
+//	00730                 if (key != K_BACKSPACE 
+//	00731                         && key != K_PAUSE 
+//	00732                         && key != K_PGUP 
+//	00733                         && key != K_KP_PGUP 
+//	00734                         && key != K_PGDN
+//	00735                         && key != K_KP_PGDN
+//	00736                         && key_repeats[key] > 1)
+//	00737                         return; // ignore most autorepeats
+//	00738                         
+//	00739                 if (key >= 200 && !keybindings[key])
+//	00740                         Com_Printf ("%s is unbound, hit F4 to set.\n", Key_KeynumToString (key) );
+//	00741         }
+//	00742         else
+//	00743         {
+//	00744                 key_repeats[key] = 0;
+//	00745         }
+//	00746 
+//	00747         if (key == K_SHIFT)
+//	00748                 shift_down = down;
+//	00749 
+//	00750         // console key is hardcoded, so the user can never unbind it
+//	00751         if (key == '`' || key == '~')
+//	00752         {
+//	00753                 if (!down)
+//	00754                         return;
+//	00755                 Con_ToggleConsole_f ();
+//	00756                 return;
+//	00757         }
+//	00758 
+//	00759         // any key during the attract mode will bring up the menu
+//	00760         if (cl.attractloop && cls.key_dest != key_menu &&
+//	00761                 !(key >= K_F1 && key <= K_F12))
+//	00762                 key = K_ESCAPE;
+//	00763 
+//	00764         // menu key is hardcoded, so the user can never unbind it
+//	00765         if (key == K_ESCAPE)
+//	00766         {
+//	00767                 if (!down)
+//	00768                         return;
+//	00769 
+//	00770                 if (cl.frame.playerstate.stats[STAT_LAYOUTS] && cls.key_dest == key_game)
+//	00771                 {       // put away help computer / inventory
+//	00772                         Cbuf_AddText ("cmd putaway\n");
+//	00773                         return;
+//	00774                 }
+//	00775                 switch (cls.key_dest)
+//	00776                 {
+//	00777                 case key_message:
+//	00778                         Key_Message (key);
+//	00779                         break;
+//	00780                 case key_menu:
+//	00781                         M_Keydown (key);
+//	00782                         break;
+//	00783                 case key_game:
+//	00784                 case key_console:
+//	00785                         M_Menu_Main_f ();
+//	00786                         break;
+//	00787                 default:
+//	00788                         Com_Error (ERR_FATAL, "Bad cls.key_dest");
+//	00789                 }
+//	00790                 return;
+//	00791         }
+//	00792 
+//	00793         // track if any key is down for BUTTON_ANY
+//	00794         keydown[key] = down;
+//	00795         if (down)
+//	00796         {
+//	00797                 if (key_repeats[key] == 1)
+//	00798                         anykeydown++;
+//	00799         }
+//	00800         else
+//	00801         {
+//	00802                 anykeydown--;
+//	00803                 if (anykeydown < 0)
+//	00804                         anykeydown = 0;
+//	00805         }
+//	00806 
+//	00807 //
+//	00808 // key up events only generate commands if the game key binding is
+//	00809 // a button command (leading + sign).  These will occur even in console mode,
+//	00810 // to keep the character from continuing an action started before a console
+//	00811 // switch.  Button commands include the kenum as a parameter, so multiple
+//	00812 // downs can be matched with ups
+//	00813 //
+//	00814         if (!down)
+//	00815         {
+//	00816                 kb = keybindings[key];
+//	00817                 if (kb && kb[0] == '+')
+//	00818                 {
+//	00819                         Com_sprintf (cmd, sizeof(cmd), "-%s %i %i\n", kb+1, key, time);
+//	00820                         Cbuf_AddText (cmd);
+//	00821                 }
+//	00822                 if (keyshift[key] != key)
+//	00823                 {
+//	00824                         kb = keybindings[keyshift[key]];
+//	00825                         if (kb && kb[0] == '+')
+//	00826                         {
+//	00827                                 Com_sprintf (cmd, sizeof(cmd), "-%s %i %i\n", kb+1, key, time);
+//	00828                                 Cbuf_AddText (cmd);
+//	00829                         }
+//	00830                 }
+//	00831                 return;
+//	00832         }
+//	00833 
+//	00834 //
+//	00835 // if not a consolekey, send to the interpreter no matter what mode is
+//	00836 //
+//	00837         if ( (cls.key_dest == key_menu && menubound[key])
+//	00838         || (cls.key_dest == key_console && !consolekeys[key])
+//	00839         || (cls.key_dest == key_game && ( cls.state == ca_active || !consolekeys[key] ) ) )
+//	00840         {
+//	00841                 kb = keybindings[key];
+//	00842                 if (kb)
+//	00843                 {
+//	00844                         if (kb[0] == '+')
+//	00845                         {       // button commands add keynum and time as a parm
+//	00846                                 Com_sprintf (cmd, sizeof(cmd), "%s %i %i\n", kb, key, time);
+//	00847                                 Cbuf_AddText (cmd);
+//	00848                         }
+//	00849                         else
+//	00850                         {
+//	00851                                 Cbuf_AddText (kb);
+//	00852                                 Cbuf_AddText ("\n");
+//	00853                         }
+//	00854                 }
+//	00855                 return;
+//	00856         }
+//	00857 
+//	00858         if (!down)
+//	00859                 return;         // other systems only care about key down events
+//	00860 
+//	00861         if (shift_down)
+//	00862                 key = keyshift[key];
+//	00863 
+//	00864         switch (cls.key_dest)
+//	00865         {
+//	00866         case key_message:
+//	00867                 Key_Message (key);
+//	00868                 break;
+//	00869         case key_menu:
+//	00870                 M_Keydown (key);
+//	00871                 break;
+//	00872 
+//	00873         case key_game:
+//	00874         case key_console:
+//	00875                 Key_Console (key);
+//	00876                 break;
+//	00877         default:
+//	00878                 Com_Error (ERR_FATAL, "Bad cls.key_dest");
+//	00879         }
+	}	
 }
---
+++ src/jake2/sys/KBD.java
@@ -0,0 +1,205 @@
+/*
+ * KBD.java
+ * Copyright (C) 2004
+ * 
+ * $Id: KBD.java,v 1.1 2004-01-07 13:07:44 hoz Exp $
+ */
+/*
+Copyright (C) 1997-2001 Id Software, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+package jake2.sys;
+
+import jake2.client.Key;
+
+import java.awt.AWTEvent;
+import java.awt.event.*;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+import java.util.LinkedList;
+
+/**
+ * KBD
+ */
+public final class KBD implements KeyListener, MouseListener {
+	
+	// modifications of eventQueue must be thread safe!
+	private static LinkedList eventQueue = new LinkedList(); 
+	
+	public static void Init() {
+//	01260         Key_Event_fp = fp;
+	}
+
+	public static void Update() {
+		// get events
+		HandleEvents();
+	}
+
+	public static void Close() {
+	}
+	
+	static void HandleEvents() {
+//		00464         XEvent event;
+//		00465         int b;
+//		00466         qboolean dowarp = false;
+//		00467         int mwx = vid.width/2;
+//		00468         int mwy = vid.height/2;
+//		00469    
+//		00470         if (!dpy)
+//		00471                 return;
+//		00472
+		Jake2InputEvent event;
+		while ( (event=nextEvent()) != null ) {
+//		00473         while (XPending(dpy)) {
+//		00474 
+//		00475                 XNextEvent(dpy, &event);
+//		00476 
+			switch(event.type) {
+				case Jake2InputEvent.KeyPress:
+				case Jake2InputEvent.KeyRelease:
+					Do_Key_Event(((KeyEvent)event.ev).getKeyCode(), event.type == Jake2InputEvent.KeyPress);
+//		00480                         if (in_state && in_state->Key_Event_fp)
+//		00481                                 in_state->Key_Event_fp (XLateKey(&event.xkey), event.type == KeyPress);
+					break;
+
+				case Jake2InputEvent.MotionNotify:
+//		00485                         if (mouse_active) {
+//		00486                                 if (dgamouse) {
+//		00487                                         mx += (event.xmotion.x + win_x) * 2;
+//		00488                                         my += (event.xmotion.y + win_y) * 2;
+//		00489                                 } 
+//		00490                                 else 
+//		00491                                 {
+//		00492                                         mx += ((int)event.xmotion.x - mwx) * 2;
+//		00493                                         my += ((int)event.xmotion.y - mwy) * 2;
+//		00494                                         mwx = event.xmotion.x;
+//		00495                                         mwy = event.xmotion.y;
+//		00496 
+//		00497                                         if (mx || my)
+//		00498                                                 dowarp = true;
+//		00499                                 }
+//		00500                         }
+					break;
+
+
+				case Jake2InputEvent.ButtonPress:
+//		00505                         b=-1;
+//		00506                         if (event.xbutton.button == 1)
+//		00507                                 b = 0;
+//		00508                         else if (event.xbutton.button == 2)
+//		00509                                 b = 2;
+//		00510                         else if (event.xbutton.button == 3)
+//		00511                                 b = 1;
+//		00512                         if (b>=0 && in_state && in_state->Key_Event_fp)
+//		00513                                 in_state->Key_Event_fp (K_MOUSE1 + b, true);
+					break;
+ 
+				case Jake2InputEvent.ButtonRelease:
+//		00517                         b=-1;
+//		00518                         if (event.xbutton.button == 1)
+//		00519                                 b = 0;
+//		00520                         else if (event.xbutton.button == 2)
+//		00521                                 b = 2;
+//		00522                         else if (event.xbutton.button == 3)
+//		00523                                 b = 1;
+//		00524                         if (b>=0 && in_state && in_state->Key_Event_fp)
+//		00525                                 in_state->Key_Event_fp (K_MOUSE1 + b, false);
+					break;
+ 
+				case Jake2InputEvent.CreateNotify :
+//		00529                         win_x = event.xcreatewindow.x;
+//		00530                         win_y = event.xcreatewindow.y;
+					break;
+
+				case Jake2InputEvent.ConfigureNotify :
+//		00534                         win_x = event.xconfigure.x;
+//		00535                         win_y = event.xconfigure.y;
+					break;
+			}
+		}
+//		00539            
+//		00540         if (dowarp) {
+//		00541                 /* move the mouse to the window center again */
+//		00542                 XWarpPointer(dpy,None,win,0,0,0,0, vid.width/2,vid.height/2);
+//		00543         }		
+	}
+	
+	private static Jake2InputEvent nextEvent() {
+		Jake2InputEvent ev;
+		synchronized (eventQueue) {
+			ev = (Jake2InputEvent)eventQueue.removeFirst();
+		}
+		return ev;
+	}
+	
+	private static void addEvent(Jake2InputEvent ev) {
+		synchronized (eventQueue) {
+			eventQueue.addLast(ev);
+		}
+	}
+	
+	static void Do_Key_Event(int key, boolean down) {
+		Key.Event(key, down, System.currentTimeMillis());
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		addEvent(new Jake2InputEvent(Jake2InputEvent.KeyPress, e));
+	}
+
+	public void keyReleased(KeyEvent e) {
+		addEvent(new Jake2InputEvent(Jake2InputEvent.KeyRelease, e));
+	}
+
+	public void keyTyped(KeyEvent e) {
+	}
+
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	public void mouseEntered(MouseEvent e) {		
+	}
+
+	public void mouseExited(MouseEvent e) {
+	}
+
+	public void mousePressed(MouseEvent e) {
+		addEvent(new Jake2InputEvent(Jake2InputEvent.ButtonPress, e));
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		addEvent(new Jake2InputEvent(Jake2InputEvent.ButtonRelease, e));
+	}
+
+}
+
+class Jake2InputEvent {
+	static final int KeyPress = 0;
+	static final int KeyRelease = 1;
+	static final int MotionNotify = 2;
+	static final int ButtonPress = 3;
+	static final int ButtonRelease = 4;
+	static final int CreateNotify = 5;
+	static final int ConfigureNotify = 6;
+	int type;
+	AWTEvent ev;
+	
+	Jake2InputEvent(int type, AWTEvent ev) {
+		this.type = type;
+		this.ev = ev;
+	}
+}
