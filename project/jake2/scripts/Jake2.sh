a2c669f3f4e2db603d319f597981f72d922b5820
Holger Zickner <hoz@bytonic.de>
2004-04-26 21:38:51 +0000

    support for joal in build and start scripts

--- project/jake2/scripts/Jake2.sh
+++ project/jake2/scripts/Jake2.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-exec java -Xmx128M -Djava.library.path=lib/linux -cp lib/jake2.jar:lib/linux/jogl.jar jake2.Jake2
+export LD_LIBRARY_PATH=lib/linux
+CP=lib/jake2.jar:lib/linux/jogl.jar:lib/linux/joal.jar
+
+exec java -Xmx128M -Djava.library.path=lib/linux -cp $CP jake2.Jake2
