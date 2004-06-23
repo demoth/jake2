114b309fdd1c2dca63356ac59760f20411b24742
Carsten Weisse <cwei@bytonic.de>
2004-06-23 12:52:42 +0000

    laed jetzt auch unsere OpenAL32.dll und zeigt die ausgaben wieder auf der Console an

--- project/jake2/scripts/Jake2.bat
+++ project/jake2/scripts/Jake2.bat
@@ -1,1 +1,3 @@
-@javaw -Xmx128M -Djava.library.path=lib/windows -cp lib/jake2.jar;lib/jogl.jar;lib/windows/joal.jar jake2.Jake2
+@echo off
+SET PATH=lib\windows;%PATH%
+java -Xmx128M -Djava.library.path=lib/windows -cp lib/jake2.jar;lib/jogl.jar;lib/windows/joal.jar jake2.Jake2
