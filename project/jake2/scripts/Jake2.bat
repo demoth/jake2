6b6ae21a9d3b27990748179783e9ff6a8ef2b72f
Carsten Weisse <cwei@bytonic.de>
2004-06-28 15:40:59 +0000

    noddraw wird in der Jake2.bat gesetzt

--- project/jake2/scripts/Jake2.bat
+++ project/jake2/scripts/Jake2.bat
@@ -1,3 +1,3 @@
 @echo off
 SET PATH=lib\windows;%PATH%
-java -Xmx128M -Djava.library.path=lib/windows -cp lib/jake2.jar;lib/jogl.jar;lib/windows/joal.jar jake2.Jake2
+java -Xmx128M -Dsun.java2d.noddraw=true -Djava.library.path=lib/windows -cp lib/jake2.jar;lib/jogl.jar;lib/windows/joal.jar jake2.Jake2
