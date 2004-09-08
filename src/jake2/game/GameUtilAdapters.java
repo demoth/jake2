abf233cb648e147b857148814fcce762b7ee5a57
Carsten Weisse <cwei@bytonic.de>
2004-09-08 20:19:12 +0000

    use only ASCII in source :-)

--- src/jake2/game/GameUtilAdapters.java
+++ src/jake2/game/GameUtilAdapters.java
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 // Created on 26.02.2004 by RST.
-// $Id: GameUtilAdapters.java,v 1.2 2004-07-09 06:50:49 hzi Exp $
+// $Id: GameUtilAdapters.java,v 1.3 2004-09-08 20:19:12 cawe Exp $
 
 package jake2.game;
 
@@ -78,7 +78,6 @@ public class GameUtilAdapters
 
 				master = ent.teammaster;
 
-				// tiefe zählen
 				// count the depth
 				for (count = 0, ent = master; ent != null; ent = ent.chain, count++);
 
