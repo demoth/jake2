3e2d4b70d8791628fe6fb91251c1bb0ec163e5dc
Carsten Weisse <cwei@bytonic.de>
2004-09-15 22:10:00 +0000

    reformat (Java conventions)

--- test/jake2/client/GhostMouse.java
+++ test/jake2/client/GhostMouse.java
@@ -1,43 +1,37 @@
 /*
-Copyright (C) 1997-2001 Id Software, Inc.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-
-See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-*/
+ * Copyright (C) 1997-2001 Id Software, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  
+ */
 
 // Created on 07.01.2000 by RST.
-// $Id: GhostMouse.java,v 1.1 2004-07-07 19:59:56 hzi Exp $
-
+// $Id: GhostMouse.java,v 1.2 2004-09-15 22:10:00 cawe Exp $
 package jake2.client;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.Dimension;
 import java.awt.Robot;
+import java.awt.Toolkit;
 
-
-// import jake2.*;
-// import jake2.client.*;
-// import jake2.game.*;
-// import jake2.qcommon.*;
-// import jake2.render.*;
-// import jake2.server.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class GhostMouse {
     public static Dimension size;
+
     public static void main(String[] args) throws Exception {
         Robot robot = new Robot();
         size = Toolkit.getDefaultToolkit().getScreenSize();
@@ -48,9 +42,8 @@ public class GhostMouse {
         button.addActionListener(new CircleListener(robot));
 
         frame.pack();
-        frame.setLocation(
-        (int)(size.getWidth()-frame.getWidth())/2,
-        (int)(size.getHeight()-frame.getHeight())/2);
+        frame.setLocation((int) (size.getWidth() - frame.getWidth()) / 2,
+                (int) (size.getHeight() - frame.getHeight()) / 2);
         frame.show();
     }
 }
