e81d4a551bfae52f729fcebb37306983efbc28e4
Carsten Weisse <cwei@bytonic.de>
2005-04-25 17:41:05 +0000

    put the lwjgl libs into classpath

--- scripts/Jake2.bat
+++ scripts/Jake2.bat
@@ -1,3 +1,4 @@
 @echo off
 SET PATH=lib\windows;%PATH%
-java -Xmx128M -Dsun.java2d.noddraw=true -Djava.library.path=lib/windows -cp lib/jake2.jar;lib/jogl.jar;lib/windows/joal.jar jake2.Jake2
+SET CP=lib/jake2.jar;lib/jogl.jar;lib/windows/joal.jar;lib/lwjgl.jar;lib/lwjgl_util.jar
+java -Xmx128M -Dsun.java2d.noddraw=true -Djava.library.path=lib/windows -cp %CP% jake2.Jake2
--- scripts/Jake2.sh
+++ scripts/Jake2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=lib/linux
-CP=lib/jake2.jar:lib/jogl.jar:lib/linux/joal.jar
+CP=lib/jake2.jar:lib/jogl.jar:lib/linux/joal.jar:lib/lwjgl.jar:lib/lwjgl_util.jar
 
 exec java -Xmx128M -Djava.library.path=lib/linux -cp $CP jake2.Jake2
