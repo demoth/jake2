19e32e801001cabaa503d11c8b2a7599106c4abd
Holger Zickner <hoz@bytonic.de>
2005-05-10 09:47:21 +0000

    jvm heap size reduced

--- scripts/Jake2.bat
+++ scripts/Jake2.bat
@@ -1,4 +1,4 @@
 @echo off
 SET PATH=lib\windows;%PATH%
 SET CP=lib/jake2.jar;lib/jogl.jar;lib/windows/joal.jar;lib/lwjgl.jar;lib/lwjgl_util.jar
-java -Xmx128M -Dsun.java2d.noddraw=true -Djava.library.path=lib/windows -cp %CP% jake2.Jake2
+java -Xmx80M -Dsun.java2d.noddraw=true -Djava.library.path=lib/windows -cp %CP% jake2.Jake2
