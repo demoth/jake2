3924f8261cab77b8e4f9b78faf9b9ded9a89c831
Holger Zickner <hoz@bytonic.de>
2005-06-06 18:22:20 +0000

    handle characters with AltGr properly

--- src/jake2/sys/InputListener.java
+++ src/jake2/sys/InputListener.java
@@ -2,7 +2,7 @@
  * InputListener.java
  * Copyright (C) 2004
  * 
- * $Id: InputListener.java,v 1.5 2005-06-06 13:30:37 hzi Exp $
+ * $Id: InputListener.java,v 1.6 2005-06-06 18:22:20 hzi Exp $
  */
 /*
 Copyright (C) 1997-2001 Id Software, Inc.
@@ -52,7 +52,9 @@ public final class InputListener implements KeyListener, MouseListener,
 	}
 
 	public void keyPressed(KeyEvent e) {
-		addEvent(new Jake2InputEvent(Jake2InputEvent.KeyPress, e));
+		if (!((e.getModifiersEx() & InputEvent.ALT_GRAPH_DOWN_MASK) != 0)) {
+			addEvent(new Jake2InputEvent(Jake2InputEvent.KeyPress, e));
+		}
 	}
 
 	public void keyReleased(KeyEvent e) {
@@ -62,6 +64,7 @@ public final class InputListener implements KeyListener, MouseListener,
 	public void keyTyped(KeyEvent e) {
 		if ((e.getModifiersEx() & InputEvent.ALT_GRAPH_DOWN_MASK) != 0) {
 			addEvent(new Jake2InputEvent(Jake2InputEvent.KeyPress, e));
+			addEvent(new Jake2InputEvent(Jake2InputEvent.KeyRelease, e));
 		}		
 	}
 
