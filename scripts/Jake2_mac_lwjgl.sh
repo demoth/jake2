2178e3a1342c289b6abbbafb96109a0e1ac76d5a
Holger Zickner <hoz@bytonic.de>
2005-10-07 06:00:48 +0000

    fixes for OS X

--- scripts/Jake2_mac_lwjgl.sh
+++ scripts/Jake2_mac_lwjgl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=lib/linux/osx
+export LD_LIBRARY_PATH=lib/osx
 CP=lib/jake2.jar:lib/lwjgl.jar:lib/lwjgl_util.jar
 
 exec java -Xmx100M -Djava.library.path=lib/osx -cp $CP jake2.Jake2
