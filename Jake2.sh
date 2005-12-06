046c3abd95082d97d67e99df5a32e51594d32620
Carsten Weisse <cwei@bytonic.de>
2005-12-06 14:56:55 +0000

    bugfix: script was wrong :-(

--- Jake2.sh
+++ Jake2.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`
 
-export LD_LIBRARY_PATH=lib/joal/linux
+export LD_LIBRARY_PATH=lib/joal/linux:lib/lwjgl/linux
 CP=build:resources:lib/jogl/jogl.jar:lib/joal/linux/joal.jar:lib/lwjgl/lwjgl.jar:lib/lwjgl/lwjgl_util.jar
 
-exec java -Xmx100M -Dlib/jogl/linux:lib/joal/linux:lib/lwjgl/linux -cp $CP jake2.Jake2
+exec java -Xmx100M -Djava.library.path=lib/jogl/linux:lib/joal/linux:lib/lwjgl/linux -cp $CP jake2.Jake2
