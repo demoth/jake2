4fb4d71deec38239c73fbfe063af6df1f0150ad9
Rene Stoeckel <rst@bytonic.de>
2005-12-10 22:10:03 +0000

    added version information

--- Jake2.sh
+++ Jake2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# $Id: Jake2.sh,v 1.3 2005-12-10 22:10:03 salomo Exp $
+
 cd `dirname $0`
 
-export LD_LIBRARY_PATH=lib/joal/linux:lib/lwjgl/linux
+export LD_LIBRARY_PATH=lib/joal/linux
 CP=build:resources:lib/jogl/jogl.jar:lib/joal/linux/joal.jar:lib/lwjgl/lwjgl.jar:lib/lwjgl/lwjgl_util.jar
 
-exec java -Xmx100M -Djava.library.path=lib/jogl/linux:lib/joal/linux:lib/lwjgl/linux -cp $CP jake2.Jake2
+exec java -Xmx100M -Dlib/jogl/linux:lib/joal/linux:lib/lwjgl/linux -cp $CP jake2.Jake2
