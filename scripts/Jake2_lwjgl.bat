4e56d048dcc7543b9c40438ea93c3c49151520e4
Carsten Weisse <cwei@bytonic.de>
2006-10-29 14:49:21 +0000

    separate lwjgl libs directory

--- scripts/Jake2_lwjgl.bat
+++ scripts/Jake2_lwjgl.bat
@@ -1,4 +1,4 @@
 @echo off
 SET PATH=lib\windows;%PATH%
 SET CP=lib/jake2.jar;lib/lwjgl.jar;lib/lwjgl_util.jar
-start javaw -Xmx100M -Dsun.java2d.noddraw=true -Djava.library.path=lib/windows -cp %CP% jake2.Jake2
+start javaw -Xmx100M -Dsun.java2d.noddraw=true -Djava.library.path=lib/windows/lwjgl -cp %CP% jake2.Jake2
--- scripts/Jake2_mac_lwjgl.sh
+++ scripts/Jake2_mac_lwjgl.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=lib/osx
+export LD_LIBRARY_PATH=lib/osx/lwjgl
+export DYLD_LIBRARY_PATH=lib/osx/lwjgl
 CP=lib/jake2.jar:lib/lwjgl.jar:lib/lwjgl_util.jar
 
-exec java -Xmx100M -Djava.library.path=lib/osx -cp $CP jake2.Jake2
+exec java -Xmx100M -Djava.library.path=lib/osx/lwjgl -cp $CP jake2.Jake2
