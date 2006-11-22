e0f156fb63c47b82f432366b569637264f5d546f
Carsten Weisse <cwei@bytonic.de>
2006-11-22 17:09:06 +0000

    bugfix: remove semicolon

--- Jake2.bat
+++ Jake2.bat
@@ -6,5 +6,5 @@ REM SET CP=build;resources;lib/lwjgl/lwjgl.jar;lib/lwjgl/lwjgl_util.jar
 
 REM for jogl and joal
 SET LIB=lib/jogl/windows;lib/joal/windows
-SET CP=build;resources;lib/jogl/jogl.jar;lib/joal/joal.jar;lib/joal/gluegen-rt.jar;
+SET CP=build;resources;lib/jogl/jogl.jar;lib/joal/joal.jar;lib/joal/gluegen-rt.jar
 java -Xmx100M -Dsun.java2d.noddraw=true -Djava.library.path=%LIB% -cp %CP% jake2.Jake2
