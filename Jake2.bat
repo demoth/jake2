8b6e92b1b32810c5d8e185147933f214c2e61e78
Carsten Weisse <cwei@bytonic.de>
2006-11-22 17:10:36 +0000

    switch to jsr231

--- Jake2.bat
+++ Jake2.bat
@@ -5,6 +5,6 @@ REM SET LIB=lib/lwjgl/windows
 REM SET CP=build;resources;lib/lwjgl/lwjgl.jar;lib/lwjgl/lwjgl_util.jar
 
 REM for jogl and joal
-SET LIB=lib/jogl/windows;lib/joal/windows
-SET CP=build;resources;lib/jogl/jogl.jar;lib/joal/joal.jar;lib/joal/gluegen-rt.jar
+SET LIB=lib/jsr231/windows;lib/joal/windows
+SET CP=build;resources;lib/jsr231/jogl.jar;lib/joal/joal.jar;lib/joal/gluegen-rt.jar
 java -Xmx100M -Dsun.java2d.noddraw=true -Djava.library.path=%LIB% -cp %CP% jake2.Jake2
