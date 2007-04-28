584af6947b6dbddce7cb34e4e80e4e8b53cb854a
Carsten Weisse <cwei@bytonic.de>
2007-04-28 18:48:46 +0000

    use gluegen-rt from jsr231 alias jogl

--- Jake2.bat
+++ Jake2.bat
@@ -6,5 +6,5 @@ REM SET CP=build;resources;lib/lwjgl/lwjgl.jar;lib/lwjgl/lwjgl_util.jar
 
 REM for jogl and joal
 SET LIB=lib/jsr231/windows;lib/joal/windows
-SET CP=build;resources;lib/jsr231/jogl.jar;lib/joal/joal.jar;lib/joal/gluegen-rt.jar
+SET CP=build;resources;lib/jsr231/jogl.jar;lib/joal/joal.jar;lib/jsr231/gluegen-rt.jar
 java -Xmx100M -Dsun.java2d.noddraw=true -Djava.library.path=%LIB% -cp %CP% jake2.Jake2
--- Jake2.sh
+++ Jake2.sh
@@ -8,6 +8,6 @@ cd `dirname $0`
 
 # for jogl and joal
 LIB=lib/jogl/linux:lib/joal/linux
-CP=build:resources:lib/jogl/jogl.jar:lib/joal/joal.jar:lib/joal/gluegen-rt.jar
+CP=build:resources:lib/jogl/jogl.jar:lib/joal/joal.jar:lib/jsr231/gluegen-rt.jar
 
 exec java -Xmx100M -Djava.library.path=$LIB -cp $CP jake2.Jake2 $*
