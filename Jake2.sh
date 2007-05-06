bde259967c739895783fc9e06f260ea1c4414c10
Carsten Weisse <cwei@bytonic.de>
2007-05-06 12:38:13 +0000

    use new jogl libs alias jsr231

--- Jake2.sh
+++ Jake2.sh
@@ -7,7 +7,7 @@ cd `dirname $0`
 #CP=build:resources:lib/lwjgl/lwjgl.jar:lib/lwjgl/lwjgl_util.jar
 
 # for jogl and joal
-LIB=lib/jogl/linux:lib/joal/linux
-CP=build:resources:lib/jogl/jogl.jar:lib/joal/joal.jar:lib/jsr231/gluegen-rt.jar
+LIB=lib/jsr231/linux:lib/joal/linux
+CP=build:resources:lib/jsr231/jogl.jar:lib/joal/joal.jar:lib/jsr231/gluegen-rt.jar
 
 exec java -Xmx100M -Djava.library.path=$LIB -cp $CP jake2.Jake2 $*
