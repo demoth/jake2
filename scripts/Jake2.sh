86a0f053b4be4f329f7dda206823285d360c9530
Rene Stoeckel <rst@bytonic.de>
2011-07-08 09:45:28 +0000

    cdaudio mp3 player support

--- scripts/Jake2.sh
+++ scripts/Jake2.sh
@@ -3,6 +3,6 @@
 cd `dirname $0`
 
 export LD_LIBRARY_PATH=lib/linux
-CP=lib/jake2.jar:lib/jogl.jar:lib/joal.jar:lib/gluegen-rt.jar
+CP=lib/jake2.jar:lib/jogl.jar:lib/joal.jar:lib/gluegen-rt.jar:lib/jl1.0.1.jar
 
 exec java -Xmx100M -Djava.library.path=lib/linux -cp $CP jake2.Jake2
--- scripts/Jake2_mac_lwjgl.sh
+++ scripts/Jake2_mac_lwjgl.sh
@@ -2,6 +2,6 @@
 
 export LD_LIBRARY_PATH=lib/osx/lwjgl
 export DYLD_LIBRARY_PATH=lib/osx/lwjgl
-CP=lib/jake2.jar:lib/lwjgl.jar:lib/lwjgl_util.jar
+CP=lib/jake2.jar:lib/lwjgl.jar:lib/lwjgl_util.jar:lib/jl1.0.1.jar
 
 exec java -Xmx100M -Djava.library.path=lib/osx/lwjgl -cp $CP jake2.Jake2
