95474a55926c1d0f2870bcbe0da70b3191c2980f
Carsten Weiße <cwei@bytonic.de>
2015-01-17 03:46:26 +0100

    support osx in the build and start script

--- Jake2.sh
+++ Jake2.sh
@@ -3,11 +3,13 @@
 cd `dirname $0`
 
 # for lwjgl
-#LIB=lib/lwjgl/linux
-#CP=build:resources:lib/lwjgl/lwjgl.jar:lib/lwjgl/lwjgl_util.jar
+if [ "$(uname)" == "Darwin" ]; then
+    LIB=lib/lwjgl/osx
+else
+    LIB=lib/lwjgl/linux
+fi
 
-# for jogl and joal
-LIB=lib/jsr231/linux:lib/joal/linux
-CP=build:resources:lib/jsr231/jogl.jar:lib/joal/joal.jar:lib/jsr231/gluegen-rt.jar
+CP=build:resources:lib/lwjgl/lwjgl.jar:lib/lwjgl/lwjgl_util.jar:lib/jl1.0.1.jar
 
 exec java -Xmx100M -Djava.library.path=$LIB -cp $CP jake2.Jake2 $*
+
--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+[ -z "$JAVA_HOME" ] && [ "$(uname)" == "Darwin" ] && JAVA_HOME=$(/usr/libexec/java_home)
+
 CP=lib/ant/ant.jar:lib/ant/ant-launcher.jar
 CP=$CP:lib/xerces/xercesImpl.jar:lib/xerces/xml-apis.jar
 CP=$CP:lib/proguard/proguard.jar
 CP=$CP:$JAVA_HOME/lib/tools.jar
 
 java -Dant.home=lib/ant -cp $CP org.apache.tools.ant.Main -buildfile build.xml $@
+
