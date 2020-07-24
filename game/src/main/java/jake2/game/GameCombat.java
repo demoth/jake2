1c534e9d3a0d5c6d58208e406fc79950a7d732dd
Daniil Bubnov <bubnov.d.e@gmail.com>
2020-07-24 20:31:38 +0200

    Fix #9 Allow running multiple instances of server on one jvm (Part 1) Game side state means of death moved to game state fixed show of scoreboard in multiplayer when players dies

--- game/src/main/java/jake2/game/GameCombat.java
+++ game/src/main/java/jake2/game/GameCombat.java
@@ -420,7 +420,7 @@ public class GameCombat {
                     mod |= GameDefines.MOD_FRIENDLY_FIRE;
             }
         }
-        GameBase.meansOfDeath = mod;
+        gameExports.meansOfDeath = mod;
     
         // easy mode takes half damage
         if (cvars.skill.value == 0 && cvars.deathmatch.value == 0
