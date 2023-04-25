6de3c44ca1ba8b652ee3c186d1508396d34a6260
Daniil Bubnov <bubnov.d.e@gmail.com>
2023-04-25 20:35:12 +0200

    Movetype component: move related functions into the component

--- game/src/main/kotlin/jake2/game/func/func_common.kt
+++ game/src/main/kotlin/jake2/game/func/func_common.kt
@@ -2,7 +2,6 @@ package jake2.game.func
 
 import jake2.game.GameDefines
 import jake2.game.GameExportsImpl
-import jake2.game.MoveInfo
 import jake2.game.SubgameEntity
 import jake2.game.adapters.EntThinkAdapter
 import jake2.game.adapters.SuperAdapter
@@ -100,9 +99,9 @@ private val accelMove: EntThinkAdapter = registerThink("thinc_accelmove") { self
     self.moveinfo.remaining_distance -= self.moveinfo.current_speed
 
     if (self.moveinfo.current_speed == 0f) // starting or blocked
-        platCalculateAcceleratedMove(self.moveinfo)
+        self.moveinfo.platCalculateAcceleratedMove()
 
-    platAccelerate(self.moveinfo)
+    self.moveinfo.platAccelerate()
 
     // will the entire move complete on next frame?
     if (self.moveinfo.remaining_distance <= self.moveinfo.current_speed) {
@@ -117,84 +116,4 @@ private val accelMove: EntThinkAdapter = registerThink("thinc_accelmove") { self
     true
 }
 
-// todo: move to moveinfo_t
-/**
- * plat_CalcAcceleratedMove 
- */
-private fun platCalculateAcceleratedMove(moveinfo: MoveInfo) {
-    moveinfo.move_speed = moveinfo.speed
-    if (moveinfo.remaining_distance < moveinfo.accel) {
-        moveinfo.current_speed = moveinfo.remaining_distance
-        return
-    }
-    val accel_dist = accelerationDistance(moveinfo.speed, moveinfo.accel)
-    var decel_dist = accelerationDistance(moveinfo.speed, moveinfo.decel)
-    if (moveinfo.remaining_distance - accel_dist - decel_dist < 0) {
-        val f = (moveinfo.accel + moveinfo.decel) / (moveinfo.accel * moveinfo.decel)
-        moveinfo.move_speed =
-            ((-2 + Math.sqrt((4 - 4 * f * (-2 * moveinfo.remaining_distance)).toDouble())) / (2 * f)).toFloat()
-        decel_dist = accelerationDistance(moveinfo.move_speed, moveinfo.decel)
-    }
-    moveinfo.decel_distance = decel_dist
-}
 
-// todo: move to moveinfo_t
-/**
- * plat_Accelerate
- */
-private fun platAccelerate(moveinfo: MoveInfo) {
-    // are we decelerating?
-    if (moveinfo.remaining_distance <= moveinfo.decel_distance) {
-        if (moveinfo.remaining_distance < moveinfo.decel_distance) {
-            if (moveinfo.next_speed != 0f) {
-                moveinfo.current_speed = moveinfo.next_speed
-                moveinfo.next_speed = 0f
-                return
-            }
-            if (moveinfo.current_speed > moveinfo.decel)
-                moveinfo.current_speed -= moveinfo.decel
-        }
-        return
-    }
-
-    // are we at full speed and need to start decelerating during this move?
-    if (moveinfo.current_speed == moveinfo.move_speed) {
-        if (moveinfo.remaining_distance - moveinfo.current_speed < moveinfo.decel_distance) {
-            val p1_distance: Float = moveinfo.remaining_distance - moveinfo.decel_distance
-            val p2_distance = moveinfo.move_speed * (1.0f - p1_distance / moveinfo.move_speed)
-            val distance = p1_distance + p2_distance
-            moveinfo.current_speed = moveinfo.move_speed
-            moveinfo.next_speed = moveinfo.move_speed - moveinfo.decel * (p2_distance / distance)
-            return
-        }
-    }
-
-    // are we accelerating?
-    if (moveinfo.current_speed < moveinfo.speed) {
-        val old_speed = moveinfo.current_speed
-
-        // figure simple acceleration up to move_speed
-        moveinfo.current_speed += moveinfo.accel
-        if (moveinfo.current_speed > moveinfo.speed) moveinfo.current_speed = moveinfo.speed
-
-        // are we accelerating throughout this entire move?
-        if (moveinfo.remaining_distance - moveinfo.current_speed >= moveinfo.decel_distance)
-            return
-
-        // during this move we will accelerate from current_speed to move_speed
-        // and cross over the decel_distance; figure the average speed for
-        // the entire move
-        val p1_distance = moveinfo.remaining_distance - moveinfo.decel_distance
-        val p1_speed = (old_speed + moveinfo.move_speed) / 2.0f
-        val p2_distance = moveinfo.move_speed * (1.0f - p1_distance / p1_speed)
-        val distance = p1_distance + p2_distance
-        moveinfo.current_speed = p1_speed * (p1_distance / distance) + moveinfo.move_speed * (p2_distance / distance)
-        moveinfo.next_speed = moveinfo.move_speed - moveinfo.decel * (p2_distance / distance)
-        return
-    }
-    // we are at constant velocity (move_speed)
-}
-
-private fun accelerationDistance(target: Float, rate: Float): Float {
-    return target * (target / rate + 1) / 2
-}
