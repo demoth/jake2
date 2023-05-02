5e4903d156096dca89891171cf75eb5a07e1bc3b
Daniil Bubnov <bubnov.d.e@gmail.com>
2023-05-02 19:21:18 +0200

    Components: MoveInfo: switch from field to component: button

--- game/src/main/java/jake2/game/SubgameEntity.java
+++ game/src/main/java/jake2/game/SubgameEntity.java
@@ -1,6 +1,7 @@
 package jake2.game;
 
 import jake2.game.adapters.*;
+import jake2.game.components.MoveInfo;
 import jake2.game.components.ThinkComponent;
 import jake2.game.items.GameItem;
 import jake2.game.items.GameItems;
@@ -101,7 +102,7 @@ public class SubgameEntity extends edict_t {
     public float[] avelocity = { 0, 0, 0 };
     public float[] move_origin = { 0, 0, 0 };
     public float[] move_angles = { 0, 0, 0 };
-    public MoveInfo moveinfo = new MoveInfo(); // move to component
+    public MoveInfo moveinfo = new MoveInfo(); //todo: move to component
 
     public int mass;
 
