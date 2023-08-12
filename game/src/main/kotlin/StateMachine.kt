6203dc719e77899180dd52e0f25d700e3e47556d
Daniil Bubnov <bubnov.d.e@gmail.com>
2023-08-12 16:52:56 +0200

    state machine drafr

--- game/src/main/kotlin/StateMachine.kt
+++ game/src/main/kotlin/StateMachine.kt
@@ -1,24 +1,84 @@
 package org.demoth
 
-abstract class State(val name: String) {
+fun interface AnimationEventProcessor {
+    fun process(events: Collection<String>)
+}
 
-    abstract fun canEnter(): Boolean
-    abstract fun canExit(): Boolean
-    abstract fun enter()
-    abstract fun update()
-    abstract fun exit(): Boolean
+abstract class State(
+    val name: String,
+    val eventProcessor: AnimationEventProcessor,
+    val nextState: String? = null
+) {
+
+    open fun canEnter() = true
+    open fun canExit() = true
+    open fun enter() {}
+    open fun update(time: Float): String? = null
+    open fun exit() = true
+}
+
+open class AnimationSequenceState(
+    name: String,
+    val animationSequence: AnimationSequence,
+    eventProcessor: AnimationEventProcessor
+) : State(name, eventProcessor) {
+    override fun canExit(): Boolean {
+        // if it's pain animation: problem: cannot die
+        return animationSequence.finished
+    }
+
+    override fun enter() {
+        animationSequence.reset()
+    }
+
+    override fun update(time: Float): String? {
+        val events = animationSequence.update(time)
+        eventProcessor.process(events)
+        if (animationSequence.finished)
+            return nextState
+
+        return null
+    }
 }
 
 class StateMachine(var currentState: State) {
-    fun update() {
-        currentState.update()
+    fun update(time: Float) {
+        val nextState = currentState.update(time)
+        if (nextState != null)
+            attemptStateChange(nextState)
     }
 
-    fun attemptStateChange(newState: State) {
+    fun attemptStateChange(newState: State): Boolean {
         if (currentState.canExit() && newState.canEnter()) {
             currentState.exit()
             currentState = newState
             newState.enter()
+            return true
+        }
+        return false
+    }
+}
+
+class StateMachine2(
+    var states: Collection<State>,
+    // from, to, check
+    var transitionMap: Map<String, Map<String, () -> Boolean>>, // can or should????
+) {
+    var currentState = states.first()
+
+    fun attemptChange(name: String) {
+        // check rules
+    }
+    fun update(time: Float) {
+        currentState.update(time)
+        val targetStates = transitionMap[currentState.name] ?: return
+        targetStates.forEach { (stateName, func) ->
+            if (func.invoke()) {
+                attemptChange(stateName)
+                return@forEach
+            }
         }
     }
 }
+// ai wants to walk, currently stunned
+// ai wants to walk, currently idle
