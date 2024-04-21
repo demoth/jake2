a6217434b43e3c71d0a40e10cdede2e537ce99c5
Daniil Bubnov <bubnov.d.e@gmail.com>
2024-04-21 21:51:25 +0200

    add simple console processing

--- cake/core/src/main/java/org/demoth/cake/stages/ConsoleStage.kt
+++ cake/core/src/main/java/org/demoth/cake/stages/ConsoleStage.kt
@@ -1,8 +1,13 @@
 package org.demoth.cake.stages
 
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.TextArea
 import com.badlogic.gdx.utils.viewport.Viewport
+import jake2.qcommon.exec.Cbuf
+import jake2.qcommon.exec.Cmd
 import ktx.scene2d.*
 
 class ConsoleStage(viewport: Viewport) : Stage(viewport) {
@@ -21,10 +26,27 @@ class ConsoleStage(viewport: Viewport) : Stage(viewport) {
                 }
                 row()
                 container {
-                    textField("console input")
+                    val consoleInput = textField()
+                    consoleInput.addListener(object : InputListener() {
+                        override fun keyUp(event: InputEvent, keycode: Int): Boolean {
+                            if (keycode == Input.Keys.ENTER) {
+                                println("Executing console input: ${consoleInput.text}")
+                                textArea.appendText("${consoleInput.text}\n")
+                                Cbuf.AddText(consoleInput.text)
+                                Cbuf.Execute()
+                                consoleInput.text = ""
+                                return true
+                            }
+                            return false
+                        }
+                    })
                     fill()
                 }
             }
+        }
+
+        Cmd.AddCommand("console_print") { args: List<String?> ->
+            textArea.appendText(args.first())
         }
     }
 }
