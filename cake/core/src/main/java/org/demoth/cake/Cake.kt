90139e5c5b4ef3f802a11bb5a757a185ee07b3b5
Daniil Bubnov <bubnov.d.e@gmail.com>
2024-04-22 14:49:54 +0200

    fix global input handling

--- cake/core/src/main/java/org/demoth/cake/Cake.kt
+++ cake/core/src/main/java/org/demoth/cake/Cake.kt
@@ -3,14 +3,13 @@ package org.demoth.cake
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.InputMultiplexer
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.utils.ScreenUtils
 import com.badlogic.gdx.utils.viewport.StretchViewport
 import jake2.qcommon.exec.Cmd
 import jake2.qcommon.exec.Cvar
 import ktx.app.KtxApplicationAdapter
+import ktx.app.KtxInputAdapter
 import ktx.scene2d.Scene2DSkin
 import org.demoth.cake.stages.ConsoleStage
 import org.demoth.cake.stages.MainMenuStage
@@ -19,9 +18,7 @@ import org.demoth.cake.stages.MainMenuStage
  * Entrypoint for the client application
  *
  */
-class Cake : KtxApplicationAdapter {
-    private lateinit var batch: SpriteBatch
-    private lateinit var image: Texture
+class Cake : KtxApplicationAdapter, KtxInputAdapter {
     private lateinit var menuStage: MainMenuStage
     private lateinit var consoleStage: ConsoleStage
     private var consoleVisible = false
@@ -34,13 +31,12 @@ class Cake : KtxApplicationAdapter {
 
     override fun create() {
         Scene2DSkin.defaultSkin = Skin(Gdx.files.internal("ui/uiskin.json"))
-        batch = SpriteBatch()
-        image = Texture("libgdx.png")
         val viewport = StretchViewport(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
         menuStage = MainMenuStage(viewport) // fixme: cvar
         consoleStage = ConsoleStage(viewport)
 
         Gdx.input.inputProcessor = InputMultiplexer(
+            this, // global input processor to control console and menu
             consoleStage,
             menuStage
         )
@@ -51,43 +47,38 @@ class Cake : KtxApplicationAdapter {
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(image, 140f, 210f)
-        batch.end()
-
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f)
 
         if (consoleVisible) {
             consoleStage.act()
             consoleStage.draw()
-        } else {
-            consoleStage.unfocusAll()
-        }
-
-        if (menuVisible) {
+        } else if (menuVisible) {
             menuStage.act()
             menuStage.draw()
-            menuStage.unfocusAll()
-        }
+        } // todo: else draw IngameScreen
+    }
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.GRAVE)) {
-            menuVisible = false
-            consoleVisible = !consoleVisible
-            if (consoleVisible) {
-                consoleStage.focus()
+    // handle ESC for menu and F1 for console
+    override fun keyUp(keycode: Int): Boolean {
+        when (keycode) {
+            Input.Keys.F1 -> {
+                menuVisible = false
+                consoleVisible = !consoleVisible
+                if (consoleVisible) {
+                    consoleStage.focus()
+                }
+                return true
             }
-        }
-
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-            consoleVisible = false
-            menuVisible = !menuVisible
+            Input.Keys.ESCAPE -> {
+                consoleVisible = false
+                menuVisible = !menuVisible
+                return true
+            }
+            else -> return false
         }
     }
 
     override fun dispose() {
-        batch.dispose()
-        image.dispose()
         menuStage.dispose()
         consoleStage.dispose()
     }
