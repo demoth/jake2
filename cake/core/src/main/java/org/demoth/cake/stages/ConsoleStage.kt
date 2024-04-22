9e4c67f7e5be1260ede8dd48db9d78c8d62b2b14
Daniil Bubnov <bubnov.d.e@gmail.com>
2024-04-22 14:15:31 +0200

    'clear' command

--- cake/core/src/main/java/org/demoth/cake/stages/ConsoleStage.kt
+++ cake/core/src/main/java/org/demoth/cake/stages/ConsoleStage.kt
@@ -5,14 +5,19 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.TextArea
+import com.badlogic.gdx.scenes.scene2d.ui.TextField
 import com.badlogic.gdx.utils.viewport.Viewport
 import jake2.qcommon.exec.Cbuf
 import jake2.qcommon.exec.Cmd
+import ktx.actors.setKeyboardFocus
 import ktx.scene2d.*
 
 class ConsoleStage(viewport: Viewport) : Stage(viewport) {
 
-    val textArea: TextArea
+    fun focus() = consoleInput.setKeyboardFocus(true)
+
+    val consoleOutput: TextArea
+    val consoleInput: TextField
 
     init {
         actors {
@@ -21,17 +26,17 @@ class ConsoleStage(viewport: Viewport) : Stage(viewport) {
                 setFillParent(true)
                 container {
                     it.growY()
-                    textArea = textArea("console text")
+                    consoleOutput = textArea("console text")
                     fill()
                 }
                 row()
                 container {
-                    val consoleInput = textField()
+                    consoleInput = textField()
                     consoleInput.addListener(object : InputListener() {
                         override fun keyUp(event: InputEvent, keycode: Int): Boolean {
                             if (keycode == Input.Keys.ENTER) {
                                 println("Executing console input: ${consoleInput.text}")
-                                textArea.appendText("${consoleInput.text}\n")
+                                consoleOutput.appendText("${consoleInput.text}\n")
                                 Cbuf.AddText(consoleInput.text)
                                 Cbuf.Execute()
                                 consoleInput.text = ""
@@ -45,8 +50,12 @@ class ConsoleStage(viewport: Viewport) : Stage(viewport) {
             }
         }
 
+        Cmd.AddCommand("clear") {
+            consoleOutput.text = ""
+        }
+
         Cmd.AddCommand("console_print") { args: List<String?> ->
-            textArea.appendText(args.first())
+            consoleOutput.appendText(args.first())
         }
     }
 }
