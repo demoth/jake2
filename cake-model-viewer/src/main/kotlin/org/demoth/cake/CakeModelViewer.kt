09bc9d5bb06e660f119c3fe7444e7ce5025a3692
Daniil Bubnov <bubnov.d.e@gmail.com>
2024-05-14 20:39:15 +0200

    move model-viewer into the jake2

---
+++ cake-model-viewer/src/main/kotlin/org/demoth/cake/CakeModelViewer.kt
@@ -0,0 +1,135 @@
+package org.demoth.cake
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.*
+import com.badlogic.gdx.graphics.GL20.GL_TRIANGLES
+import com.badlogic.gdx.graphics.VertexAttributes.Usage
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g3d.*
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight
+import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader
+import com.badlogic.gdx.graphics.g3d.model.Node
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder
+import com.badlogic.gdx.utils.UBJsonReader
+import ktx.graphics.use
+
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms.  */
+class CakeModelViewer : ApplicationAdapter() {
+    private lateinit var batch: SpriteBatch
+    private lateinit var image: Texture
+    private lateinit var modelBatch: ModelBatch
+    private lateinit var camera: Camera
+    private lateinit var models: MutableList<ModelInstance>
+    private lateinit var environment: Environment
+
+    override fun create() {
+        batch = SpriteBatch()
+        image = Texture("libgdx.png")
+        modelBatch = ModelBatch()
+        camera = PerspectiveCamera(90f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+        camera.position.set(0f, 0f, -5f);
+        camera.lookAt(0f, 0f, 0f);
+        camera.near = 0.1f;
+        camera.far = 300f;
+
+        Gdx.input.inputProcessor = CameraInputController(camera)
+        modelBatch = ModelBatch()
+        val md2Model = Md2ModelLoader().loadMd2Model("tris.md2", "viper.png")
+        models = mutableListOf(
+//            ModelInstance(createModel()),
+//            loadObjModel(),
+//            loadG3dModel(),
+//            loadMeshModel(),
+            md2Model
+        )
+
+        environment = Environment()
+//        environment.set(ColorAttribute(ColorAttribute.AmbientLight, 0.8f, 0.8f, 0.8f, 1f))
+        environment.add(DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f))
+    }
+
+    /**
+     * Create a model programmatically using the model builder
+     */
+    private fun createModel(): Model {
+        val modelBuilder = ModelBuilder()
+        val box = modelBuilder.createBox(
+            2f, 2f, 2f,
+            Material(
+                ColorAttribute.createDiffuse(Color.BLUE)
+//                todo: add texture and check coords
+            ),
+            (Usage.Position or Usage.Normal).toLong()
+        )
+        return box
+    }
+
+    /**
+     * Load an .obj model. todo: try using a material
+     */
+    private fun loadObjModel(): ModelInstance {
+        val suzanneModel = ObjLoader().loadModel(Gdx.files.internal("suzanne.obj"))
+        val suzanne = ModelInstance(suzanneModel)
+        suzanne.transform.translate(0f, 2f, 0f)
+        return suzanne
+    }
+
+    /**
+     * Example of building the model with the mesh builder class (from geometric shapes)
+     */
+    private fun loadMeshModel(): ModelInstance {
+        val modelBuilder = ModelBuilder()
+        modelBuilder.begin()
+        var meshBuilder =
+            modelBuilder.part("part1", GL_TRIANGLES, (Usage.Position or Usage.Normal).toLong(), Material())
+        meshBuilder.cone(5f, 5f, 5f, 10)
+        val node: Node = modelBuilder.node()
+        node.translation.set(10f, 0f, 0f)
+        meshBuilder = modelBuilder.part("part2", GL_TRIANGLES, (Usage.Position or Usage.Normal).toLong(), Material())
+        meshBuilder.sphere(5f, 5f, 5f, 10, 10)
+        val model = modelBuilder.end()
+        return ModelInstance(model)
+    }
+
+    /**
+     * Load an .g3d model, created with the fbx-conv app from an .fbx file
+     */
+    private fun loadG3dModel(): ModelInstance {
+        val crateModel = G3dModelLoader(UBJsonReader()).loadModel(Gdx.files.internal("crate-wooden.g3db"))
+        val crateInstance = ModelInstance(crateModel)
+        crateInstance.transform.translate(0f, -2f, 0f)
+        crateInstance.transform.scale(0.01f, 0.01f, 0.01f)
+        return crateInstance
+    }
+
+
+
+    override fun render() {
+        Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
+
+        batch.use {
+            it.draw(image, 140f, 210f)
+        }
+
+        camera.update();
+
+        modelBatch.begin(camera)
+        models.forEach {
+            modelBatch.render(it, environment)
+        }
+        modelBatch.end()
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        image.dispose()
+        models.forEach { it.model.dispose() }
+        modelBatch.dispose()
+    }
+}
---
+++ cake-model-viewer/src/main/kotlin/org/demoth/cake/Md2Format.kt
@@ -0,0 +1,339 @@
+package org.demoth.cake
+
+import java.nio.ByteBuffer
+
+/**
+ * daliasframe_t
+ * A frame in the MD2 model
+ */
+
+data class Point(
+    val x: Int,
+    val y: Int,
+    val z: Int,
+    val normalIndex: Int)
+
+/**
+ * Header:
+ *  gl cmd: +3, s1 t1 i1, s2 t2 i2, s3 t3 i3,
+ *          -5 i1 i2 i3 i4 i5
+ *   * ---- *
+ *   |\     |
+ *   | \    |
+ *   |  \   |
+ *   |   \  |
+ *   |    \ |
+ *   * ---- *
+ */
+
+class Md2Frame(buffer: ByteBuffer, num_xyz: Int) {
+    var scale = floatArrayOf(0f, 0f, 0f) // multiply byte verts by this
+    var translate = floatArrayOf(0f, 0f, 0f) // then add this
+    var name // frame name from grabbing (size 16)
+            : String
+    var verts: IntArray
+    val points: Array<Point>
+
+    init {
+        scale[0] = buffer.float
+        scale[1] = buffer.float
+        scale[2] = buffer.float
+        translate[0] = buffer.float
+        translate[1] = buffer.float
+        translate[2] = buffer.float
+        val nameBuf = ByteArray(16)
+        buffer[nameBuf]
+        name = String(nameBuf).trim { it <= ' ' }
+
+        // vertices are all 8 bit, so no swapping needed
+        verts = IntArray(num_xyz)
+        points = Array(num_xyz) { Point(0, 0, 0, 0) }
+
+
+        for (k in 0 until num_xyz) {
+            verts[k] = buffer.int
+            points[k] = Point(
+                verts[k] ushr 0 and 0xFF,
+                verts[k] ushr 8 and 0xFF,
+                verts[k] ushr 16 and 0xFF,
+                verts[k] ushr 24 and 0xFF
+            )
+        }
+    }
+}
+
+/**
+ * MD2 (quake2) model (previously named dmdl_t)
+ *
+ *
+ * Also known as "alias model" in the code - named after the model editor originally used (PowerAnimator)
+ *
+ *
+ * Header section contains size information (how many vertices, frames, skins, etc)
+ * and byte offsets to the respective sections.
+ * Body section are the data arrays.
+ *
+ *
+ * The GL commands (glcmd) format:
+ *
+ *  *  positive integer starts a tristrip command, followed by that many vertex structures.
+ *  *  negative integer starts a trifan command, followed by -x vertexes
+ *  *  zero indicates the end of the command list.
+ *
+ *
+ *
+ * A vertex consists of a floating point s, a floating point and an integer vertex index.
+ */
+class Md2Model(b: ByteBuffer) {
+    // Header
+    var ident: Int
+    var version: Int
+    var num_skins: Int
+    var num_vertices: Int
+    var num_tris: Int
+    var num_glcmds: Int // dwords in strip/fan command list
+
+    var num_frames: Int
+
+    // each skin is a MAX_SKINNAME string
+    var skinsOffset: Int
+    var firstFrameOffset: Int
+    var glCommandsOffset: Int
+    var endOfFileOffset: Int // end of file
+
+
+    // Body
+    var skinNames: Array<String?> = arrayOf()
+    var glCmds: IntArray = intArrayOf()
+
+    // all frames have vertex array of equal size (num_xyz)
+    var frames: Array<Md2Frame?> = arrayOf()
+
+    constructor(b: ByteBuffer, modelName: String) : this(b) {
+        loadBody(b)
+    }
+
+    /**
+     * Reads md2 header
+     */
+    init {
+        ident = b.int
+        version = b.int
+        b.int // skinwidth
+        b.int // skinheight
+        b.int // framesize: byte size of each frame
+        num_skins = b.int
+        num_vertices = b.int
+        b.int //num_st: greater than num_xyz for seams, parsed into dstvert_t
+        num_tris = b.int
+        num_glcmds = b.int // dwords in strip/fan command list
+        num_frames = b.int
+        skinsOffset = b.int // each skin is a MAX_SKINNAME string
+        b.int // offset from start until dstvert_t[]
+        b.int // offset from start until dtriangles[]
+        firstFrameOffset = b.int // offset for first frame
+        glCommandsOffset = b.int
+        endOfFileOffset = b.int // end of file
+    }
+
+    /**
+     * Loads the bulk of the data
+     */
+    private fun loadBody(buffer: ByteBuffer) {
+        //
+        //	   load the frames
+        //
+        frames = arrayOfNulls(num_frames)
+        buffer.position(firstFrameOffset)
+        for (i in 0 until num_frames) {
+            frames[i] = Md2Frame(buffer, num_vertices)
+        }
+
+        //
+        // load the glcmds
+        // STRIP or FAN
+        glCmds = IntArray(num_glcmds)
+        buffer.position(glCommandsOffset)
+        for (i in 0 until num_glcmds) {
+            glCmds[i] = buffer.int
+        }
+        skinNames = arrayOfNulls(num_skins)
+        val nameBuf = ByteArray(64)
+        buffer.position(skinsOffset)
+        for (i in 0 until num_skins) {
+            buffer[nameBuf]
+            skinNames[i] = String(nameBuf)
+            val n = skinNames[i]!!.indexOf('\u0000')
+            if (n > -1) {
+                skinNames[i] = skinNames[i]!!.substring(0, n)
+            }
+        }
+    }
+}
+
+val VERTEXNORMALS: Array<FloatArray> = arrayOf(
+    floatArrayOf(-0.525731f, 0.000000f, 0.850651f),
+    floatArrayOf(-0.442863f, 0.238856f, 0.864188f),
+    floatArrayOf(-0.295242f, 0.000000f, 0.955423f),
+    floatArrayOf(-0.309017f, 0.500000f, 0.809017f),
+    floatArrayOf(-0.162460f, 0.262866f, 0.951056f),
+    floatArrayOf(0.000000f, 0.000000f, 1.000000f),
+    floatArrayOf(0.000000f, 0.850651f, 0.525731f),
+    floatArrayOf(-0.147621f, 0.716567f, 0.681718f),
+    floatArrayOf(0.147621f, 0.716567f, 0.681718f),
+    floatArrayOf(0.000000f, 0.525731f, 0.850651f),
+    floatArrayOf(0.309017f, 0.500000f, 0.809017f),
+    floatArrayOf(0.525731f, 0.000000f, 0.850651f),
+    floatArrayOf(0.295242f, 0.000000f, 0.955423f),
+    floatArrayOf(0.442863f, 0.238856f, 0.864188f),
+    floatArrayOf(0.162460f, 0.262866f, 0.951056f),
+    floatArrayOf(-0.681718f, 0.147621f, 0.716567f),
+    floatArrayOf(-0.809017f, 0.309017f, 0.500000f),
+    floatArrayOf(-0.587785f, 0.425325f, 0.688191f),
+    floatArrayOf(-0.850651f, 0.525731f, 0.000000f),
+    floatArrayOf(-0.864188f, 0.442863f, 0.238856f),
+    floatArrayOf(-0.716567f, 0.681718f, 0.147621f),
+    floatArrayOf(-0.688191f, 0.587785f, 0.425325f),
+    floatArrayOf(-0.500000f, 0.809017f, 0.309017f),
+    floatArrayOf(-0.238856f, 0.864188f, 0.442863f),
+    floatArrayOf(-0.425325f, 0.688191f, 0.587785f),
+    floatArrayOf(-0.716567f, 0.681718f, -0.147621f),
+    floatArrayOf(-0.500000f, 0.809017f, -0.309017f),
+    floatArrayOf(-0.525731f, 0.850651f, 0.000000f),
+    floatArrayOf(0.000000f, 0.850651f, -0.525731f),
+    floatArrayOf(-0.238856f, 0.864188f, -0.442863f),
+    floatArrayOf(0.000000f, 0.955423f, -0.295242f),
+    floatArrayOf(-0.262866f, 0.951056f, -0.162460f),
+    floatArrayOf(0.000000f, 1.000000f, 0.000000f),
+    floatArrayOf(0.000000f, 0.955423f, 0.295242f),
+    floatArrayOf(-0.262866f, 0.951056f, 0.162460f),
+    floatArrayOf(0.238856f, 0.864188f, 0.442863f),
+    floatArrayOf(0.262866f, 0.951056f, 0.162460f),
+    floatArrayOf(0.500000f, 0.809017f, 0.309017f),
+    floatArrayOf(0.238856f, 0.864188f, -0.442863f),
+    floatArrayOf(0.262866f, 0.951056f, -0.162460f),
+    floatArrayOf(0.500000f, 0.809017f, -0.309017f),
+    floatArrayOf(0.850651f, 0.525731f, 0.000000f),
+    floatArrayOf(0.716567f, 0.681718f, 0.147621f),
+    floatArrayOf(0.716567f, 0.681718f, -0.147621f),
+    floatArrayOf(0.525731f, 0.850651f, 0.000000f),
+    floatArrayOf(0.425325f, 0.688191f, 0.587785f),
+    floatArrayOf(0.864188f, 0.442863f, 0.238856f),
+    floatArrayOf(0.688191f, 0.587785f, 0.425325f),
+    floatArrayOf(0.809017f, 0.309017f, 0.500000f),
+    floatArrayOf(0.681718f, 0.147621f, 0.716567f),
+    floatArrayOf(0.587785f, 0.425325f, 0.688191f),
+    floatArrayOf(0.955423f, 0.295242f, 0.000000f),
+    floatArrayOf(1.000000f, 0.000000f, 0.000000f),
+    floatArrayOf(0.951056f, 0.162460f, 0.262866f),
+    floatArrayOf(0.850651f, -0.525731f, 0.000000f),
+    floatArrayOf(0.955423f, -0.295242f, 0.000000f),
+    floatArrayOf(0.864188f, -0.442863f, 0.238856f),
+    floatArrayOf(0.951056f, -0.162460f, 0.262866f),
+    floatArrayOf(0.809017f, -0.309017f, 0.500000f),
+    floatArrayOf(0.681718f, -0.147621f, 0.716567f),
+    floatArrayOf(0.850651f, 0.000000f, 0.525731f),
+    floatArrayOf(0.864188f, 0.442863f, -0.238856f),
+    floatArrayOf(0.809017f, 0.309017f, -0.500000f),
+    floatArrayOf(0.951056f, 0.162460f, -0.262866f),
+    floatArrayOf(0.525731f, 0.000000f, -0.850651f),
+    floatArrayOf(0.681718f, 0.147621f, -0.716567f),
+    floatArrayOf(0.681718f, -0.147621f, -0.716567f),
+    floatArrayOf(0.850651f, 0.000000f, -0.525731f),
+    floatArrayOf(0.809017f, -0.309017f, -0.500000f),
+    floatArrayOf(0.864188f, -0.442863f, -0.238856f),
+    floatArrayOf(0.951056f, -0.162460f, -0.262866f),
+    floatArrayOf(0.147621f, 0.716567f, -0.681718f),
+    floatArrayOf(0.309017f, 0.500000f, -0.809017f),
+    floatArrayOf(0.425325f, 0.688191f, -0.587785f),
+    floatArrayOf(0.442863f, 0.238856f, -0.864188f),
+    floatArrayOf(0.587785f, 0.425325f, -0.688191f),
+    floatArrayOf(0.688191f, 0.587785f, -0.425325f),
+    floatArrayOf(-0.147621f, 0.716567f, -0.681718f),
+    floatArrayOf(-0.309017f, 0.500000f, -0.809017f),
+    floatArrayOf(0.000000f, 0.525731f, -0.850651f),
+    floatArrayOf(-0.525731f, 0.000000f, -0.850651f),
+    floatArrayOf(-0.442863f, 0.238856f, -0.864188f),
+    floatArrayOf(-0.295242f, 0.000000f, -0.955423f),
+    floatArrayOf(-0.162460f, 0.262866f, -0.951056f),
+    floatArrayOf(0.000000f, 0.000000f, -1.000000f),
+    floatArrayOf(0.295242f, 0.000000f, -0.955423f),
+    floatArrayOf(0.162460f, 0.262866f, -0.951056f),
+    floatArrayOf(-0.442863f, -0.238856f, -0.864188f),
+    floatArrayOf(-0.309017f, -0.500000f, -0.809017f),
+    floatArrayOf(-0.162460f, -0.262866f, -0.951056f),
+    floatArrayOf(0.000000f, -0.850651f, -0.525731f),
+    floatArrayOf(-0.147621f, -0.716567f, -0.681718f),
+    floatArrayOf(0.147621f, -0.716567f, -0.681718f),
+    floatArrayOf(0.000000f, -0.525731f, -0.850651f),
+    floatArrayOf(0.309017f, -0.500000f, -0.809017f),
+    floatArrayOf(0.442863f, -0.238856f, -0.864188f),
+    floatArrayOf(0.162460f, -0.262866f, -0.951056f),
+    floatArrayOf(0.238856f, -0.864188f, -0.442863f),
+    floatArrayOf(0.500000f, -0.809017f, -0.309017f),
+    floatArrayOf(0.425325f, -0.688191f, -0.587785f),
+    floatArrayOf(0.716567f, -0.681718f, -0.147621f),
+    floatArrayOf(0.688191f, -0.587785f, -0.425325f),
+    floatArrayOf(0.587785f, -0.425325f, -0.688191f),
+    floatArrayOf(0.000000f, -0.955423f, -0.295242f),
+    floatArrayOf(0.000000f, -1.000000f, 0.000000f),
+    floatArrayOf(0.262866f, -0.951056f, -0.162460f),
+    floatArrayOf(0.000000f, -0.850651f, 0.525731f),
+    floatArrayOf(0.000000f, -0.955423f, 0.295242f),
+    floatArrayOf(0.238856f, -0.864188f, 0.442863f),
+    floatArrayOf(0.262866f, -0.951056f, 0.162460f),
+    floatArrayOf(0.500000f, -0.809017f, 0.309017f),
+    floatArrayOf(0.716567f, -0.681718f, 0.147621f),
+    floatArrayOf(0.525731f, -0.850651f, 0.000000f),
+    floatArrayOf(-0.238856f, -0.864188f, -0.442863f),
+    floatArrayOf(-0.500000f, -0.809017f, -0.309017f),
+    floatArrayOf(-0.262866f, -0.951056f, -0.162460f),
+    floatArrayOf(-0.850651f, -0.525731f, 0.000000f),
+    floatArrayOf(-0.716567f, -0.681718f, -0.147621f),
+    floatArrayOf(-0.716567f, -0.681718f, 0.147621f),
+    floatArrayOf(-0.525731f, -0.850651f, 0.000000f),
+    floatArrayOf(-0.500000f, -0.809017f, 0.309017f),
+    floatArrayOf(-0.238856f, -0.864188f, 0.442863f),
+    floatArrayOf(-0.262866f, -0.951056f, 0.162460f),
+    floatArrayOf(-0.864188f, -0.442863f, 0.238856f),
+    floatArrayOf(-0.809017f, -0.309017f, 0.500000f),
+    floatArrayOf(-0.688191f, -0.587785f, 0.425325f),
+    floatArrayOf(-0.681718f, -0.147621f, 0.716567f),
+    floatArrayOf(-0.442863f, -0.238856f, 0.864188f),
+    floatArrayOf(-0.587785f, -0.425325f, 0.688191f),
+    floatArrayOf(-0.309017f, -0.500000f, 0.809017f),
+    floatArrayOf(-0.147621f, -0.716567f, 0.681718f),
+    floatArrayOf(-0.425325f, -0.688191f, 0.587785f),
+    floatArrayOf(-0.162460f, -0.262866f, 0.951056f),
+    floatArrayOf(0.442863f, -0.238856f, 0.864188f),
+    floatArrayOf(0.162460f, -0.262866f, 0.951056f),
+    floatArrayOf(0.309017f, -0.500000f, 0.809017f),
+    floatArrayOf(0.147621f, -0.716567f, 0.681718f),
+    floatArrayOf(0.000000f, -0.525731f, 0.850651f),
+    floatArrayOf(0.425325f, -0.688191f, 0.587785f),
+    floatArrayOf(0.587785f, -0.425325f, 0.688191f),
+    floatArrayOf(0.688191f, -0.587785f, 0.425325f),
+    floatArrayOf(-0.955423f, 0.295242f, 0.000000f),
+    floatArrayOf(-0.951056f, 0.162460f, 0.262866f),
+    floatArrayOf(-1.000000f, 0.000000f, 0.000000f),
+    floatArrayOf(-0.850651f, 0.000000f, 0.525731f),
+    floatArrayOf(-0.955423f, -0.295242f, 0.000000f),
+    floatArrayOf(-0.951056f, -0.162460f, 0.262866f),
+    floatArrayOf(-0.864188f, 0.442863f, -0.238856f),
+    floatArrayOf(-0.951056f, 0.162460f, -0.262866f),
+    floatArrayOf(-0.809017f, 0.309017f, -0.500000f),
+    floatArrayOf(-0.864188f, -0.442863f, -0.238856f),
+    floatArrayOf(-0.951056f, -0.162460f, -0.262866f),
+    floatArrayOf(-0.809017f, -0.309017f, -0.500000f),
+    floatArrayOf(-0.681718f, 0.147621f, -0.716567f),
+    floatArrayOf(-0.681718f, -0.147621f, -0.716567f),
+    floatArrayOf(-0.850651f, 0.000000f, -0.525731f),
+    floatArrayOf(-0.688191f, 0.587785f, -0.425325f),
+    floatArrayOf(-0.587785f, 0.425325f, -0.688191f),
+    floatArrayOf(-0.425325f, 0.688191f, -0.587785f),
+    floatArrayOf(-0.425325f, -0.688191f, -0.587785f),
+    floatArrayOf(-0.587785f, -0.425325f, -0.688191f),
+    floatArrayOf(-0.688191f, -0.587785f, -0.425325f)
+)
+
+
