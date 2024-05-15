53d7f8d12d8633e0956f2dd5aab8283e2c12932b
Daniil Bubnov <bubnov.d.e@gmail.com>
2024-05-16 00:06:08 +0200

    fix model viewer launcher

--- cake-model-viewer/src/main/kotlin/org/demoth/cake/modelviewer/Lwjgl3Launcher.kt
+++ cake-model-viewer/src/main/kotlin/org/demoth/cake/modelviewer/Lwjgl3Launcher.kt
@@ -3,10 +3,13 @@ package org.demoth.cake.modelviewer
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 
-fun main() {
-    if (StartupHelper.startNewJvmIfRequired()) return  // This handles macOS support and helps on Windows.
+object Lwjgl3Launcher {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        if (StartupHelper.startNewJvmIfRequired()) return  // This handles macOS support and helps on Windows.
 
-    createApplication()
+        createApplication()
+    }
 }
 
 private fun createApplication(): Lwjgl3Application {
@@ -24,6 +27,6 @@ private val defaultConfiguration: Lwjgl3ApplicationConfiguration
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
         configuration.setWindowedMode(640, 480)
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png")
+//        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png")
         return configuration
     }
