ae0aedcd2eb68d8d5da7f184ed871ed19cedb020
Daniil Bubnov <bubnov.d.e@gmail.com>
2024-05-21 23:15:33 +0200

    Model viewer: .wal support #112
    parse .wal file, add test

---
+++ qcommon/src/main/kotlin/jake2/qcommon/filesystem/wal.kt
@@ -0,0 +1,74 @@
+package jake2.qcommon.filesystem
+
+import java.nio.ByteBuffer
+
+/**
+ * Quake2 WAL texture image format (miptex_t)
+ *
+ * Quake 2 stores textures in a proprietary 2D image format called WAL.
+*
+ * WAL textures are stored in a 8-bit indexed color format with a specific palette being used by all textures
+ * (this palette is stored in the PAK data file that comes with Quake 2).
+ * Four mip-map levels are stored for each texture at sizes decreasing by a factor of two.
+ * This is mostly for software rendering since most 3D APIs will automatically generate the mip-map levels
+ * when you create a texture. Each frame of an animated texture is stored as an individual WAL file,
+ * and the animation sequence is encoded by storing the name of the next texture in the sequence for each frame;
+ * texture names are stored with paths and without any extension.
+ *
+ * The actual texture data is stored in an 8-bits-per-pixel raw format in a left-right, top-down order.
+ *
+ */
+class WAL(buffer: ByteBuffer) {
+    //
+    // HEADER
+    //
+    /**
+     * name of the texture, 32 bytes
+     */
+    val name: String
+    /**
+     * width (in pixels) of the largest mipmap level
+     */
+    val width: Int // fixme: unsigned
+    /**
+     * height (in pixels) of the largest mipmap level
+     */
+    val height: Int // fixme: unsigned
+    /**
+     * byte offsets of the start of each of the 4 mipmap levels, starting with largest
+     */
+    val offsets: IntArray
+
+    /**
+     * name of the next texture in the animation, 32 bytes
+     */
+    val nextName: String
+
+    // TODO:
+    val flags: Int // fixme: unsigned
+    val contents: Int  // fixme: unsigned
+    val value: Int  // fixme: unsigned
+
+    //
+    // BODY
+    //
+    val pixelData: ByteArray
+
+    init {
+        name = String(ByteArray(32).also { buffer.get(it) }).trim { it < ' '}
+        width = buffer.getInt()
+        height = buffer.getInt()
+        offsets = IntArray(MIP_LEVELS) { buffer.getInt() }
+        nextName = String(ByteArray(32).also { buffer.get(it) }).trim { it < ' '}
+        flags = buffer.getInt()
+        contents = buffer.getInt()
+        value = buffer.getInt()
+
+        // read the largest MIP
+        buffer.position(offsets.first())
+        pixelData = ByteArray(width * height)
+        buffer.get(pixelData)
+    }
+}
+
+const val MIP_LEVELS = 4
---
+++ qcommon/src/test/kotlin/jake2/qcommon/filesystem/WalTest.kt
@@ -0,0 +1,25 @@
+package jake2.qcommon.filesystem
+
+import org.junit.Assert.assertArrayEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+class WalTest {
+    @Test
+    fun loadWal() {
+        val wal = WAL(
+            ByteBuffer.wrap(this::class.java.getResourceAsStream("dc1a.wal")!!.readBytes())
+                .also { it.order(ByteOrder.LITTLE_ENDIAN) })
+
+        assertEquals("e7bm",wal.name)
+        assertEquals(128,wal.width)
+        assertEquals(32,wal.height)
+        assertArrayEquals(wal.offsets, intArrayOf(100, 4196, 5220, 5476))
+        assertEquals(0, wal.flags)
+        assertEquals(0, wal.contents)
+        assertEquals(0, wal.value)
+        assertEquals(128 * 32, wal.pixelData.size)
+    }
+}
