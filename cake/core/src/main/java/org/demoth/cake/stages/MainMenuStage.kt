dc702d779f73e318fc8a94d1437c356f358e5b25
Daniil Bubnov <bubnov.d.e@gmail.com>
2024-06-16 14:34:18 +0200

    main menu: multiplayer connect

--- cake/core/src/main/java/org/demoth/cake/stages/MainMenuStage.kt
+++ cake/core/src/main/java/org/demoth/cake/stages/MainMenuStage.kt
@@ -3,6 +3,7 @@ package org.demoth.cake.stages
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.Viewport
+import jake2.qcommon.exec.Cbuf
 import ktx.actors.onClick
 import ktx.scene2d.actors
 import ktx.scene2d.label
@@ -20,7 +21,11 @@ class MainMenuStage(viewport: Viewport) : Stage(viewport) {
                 setFillParent(true)
                 textButton("Single player")
                 row()
-                textButton("Multiplayer")
+                textButton("Multiplayer") {
+                    onClick {
+                        Cbuf.AddText("connect 127.0.0.1")
+                    }
+                }
                 row()
                 textButton("Settings")
                 row()
