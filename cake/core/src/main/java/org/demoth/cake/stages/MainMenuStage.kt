10343188f2a836b5b958f9379e7867599269754e
Daniil Bubnov <bubnov.d.e@gmail.com>
2024-06-16 14:31:38 +0200

    main menu: add bogus main menu

--- cake/core/src/main/java/org/demoth/cake/stages/MainMenuStage.kt
+++ cake/core/src/main/java/org/demoth/cake/stages/MainMenuStage.kt
@@ -1,9 +1,13 @@
 package org.demoth.cake.stages
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.Viewport
+import ktx.actors.onClick
 import ktx.scene2d.actors
 import ktx.scene2d.label
+import ktx.scene2d.table
+import ktx.scene2d.textButton
 
 /**
  * Prototype for other stages
@@ -11,6 +15,21 @@ import ktx.scene2d.label
 class MainMenuStage(viewport: Viewport) : Stage(viewport) {
     init {
         actors {
+            table {
+                defaults().pad(8f)
+                setFillParent(true)
+                textButton("Single player")
+                row()
+                textButton("Multiplayer")
+                row()
+                textButton("Settings")
+                row()
+                textButton("Exit") {
+                    onClick {
+                        Gdx.app.exit()
+                    }
+                }
+            }
             label("version: 0.0.1")
         }
     }
