cb5eb432e7bf150da7f9daacb4899378402cca2d
Daniil Bubnov <bubnov.d.e@gmail.com>
2025-01-31 21:54:26 +0100

    feat: enable anti-aliasing

--- cake/lwjgl3/src/main/kotlin/org/demoth/cake/lwjgl3/Lwjgl3GameLauncher.kt
+++ cake/lwjgl3/src/main/kotlin/org/demoth/cake/lwjgl3/Lwjgl3GameLauncher.kt
@@ -19,12 +19,21 @@ object Lwjgl3GameLauncher {
     private fun getDefaultConfiguration(): Lwjgl3ApplicationConfiguration {
         return Lwjgl3ApplicationConfiguration().apply {
             setTitle("cake-engine")
+            setBackBufferConfig(
+                /* r = */ 8,
+                /* g = */ 8,
+                /* b = */ 8,
+                /* a = */ 8,
+                /* depth = */ 24,
+                /* stencil = */ 0,
+                /* samples = */ 4 // anti-aliasing
+            )
 
             // todo: load displaymode from the configuration
             useVsync(true)
             setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate)
             setResizable(false)
-            setWindowedMode(640, 480)
+            setWindowedMode(1200, 900)
 //            setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode())
             setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png")
         }
