af53286206837b8d4d0c3af30f7dd16988822d3f
Daniil Bubnov <bubnov.d.e@gmail.com>
2025-02-13 20:00:17 +0100

    feat: md2 animation WIP:
    create a wrapper around libgdx model, load and keep all vertex locations
     for all frames to have ability to set vertices at runtime
    
     TODO: how to change model mesh?

--- cake/core/src/main/kotlin/org/demoth/cake/modelviewer/Md2ModelLoader.kt
+++ cake/core/src/main/kotlin/org/demoth/cake/modelviewer/Md2ModelLoader.kt
@@ -8,11 +8,72 @@ import com.badlogic.gdx.graphics.g3d.Material
 import com.badlogic.gdx.graphics.g3d.Model
 import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder
+import com.badlogic.gdx.utils.Disposable
 import jake2.qcommon.filesystem.Md2Model
 import jake2.qcommon.filesystem.PCX
 import org.demoth.cake.ResourceLocator
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+
+class CakeMd2Model(
+    private val locator: ResourceLocator,
+    private val modelName: String,
+    private val playerSkin: String? = null,
+): Disposable {
+    val model: Model
+    private val vertexBuffers: List<List<Float>>
+
+    init {
+        val findModel = locator.findModel(modelName) ?: throw IllegalStateException("Model $modelName not found")
+        val md2Model: Md2Model = readMd2Model(findModel)
+
+        val skins = md2Model.skinNames.map {
+            locator.findSkin(it)
+        }
+
+        // buffers for all frames
+        vertexBuffers = md2Model.frames.map { frame ->
+            md2Model.glCommands.flatMap {
+                it.toFloats(frame.points)
+            }
+        }
+
+        val modelBuilder = ModelBuilder()
+        modelBuilder.begin()
+        val modelSkin: ByteArray = if (skins.isNotEmpty()) {
+            skins[0]
+        } else {
+            if (playerSkin != null) {
+                locator.findSkin(playerSkin)
+            } else throw IllegalStateException("No skin found in the model, no player skin provided")
+        }
+        val meshBuilder = modelBuilder.part(
+            "part1",
+            GL_TRIANGLES,
+            VertexAttributes(VertexAttribute.Position(), VertexAttribute.TexCoords(0)),
+            Material(
+                TextureAttribute(
+                    TextureAttribute.Diffuse,
+                    Texture(PCXTextureData(fromPCX(PCX(modelSkin)))),
+                )
+            )
+        )
+        // initialize with the first frame, will be changed later
+        val size = vertexBuffers.first().size / 5 // 5 floats per vertex : fixme: not great
+        meshBuilder.addMesh(vertexBuffers.first().toFloatArray(), (0..<size).map { it.toShort() }.toShortArray())
+        model = modelBuilder.end()
+    }
+
+    fun setFrame(frameIndex: Int) {
+        check(frameIndex < vertexBuffers.size) { "Frame index out of bounds" }
+        // assume only one mesh
+        model.meshes.first().setVertices(vertexBuffers[frameIndex].toFloatArray())
+    }
+
+    override fun dispose() {
+        model.dispose()
+    }
+}
 
 class Md2ModelLoader(val locator: ResourceLocator) {
     fun loadMd2Model(
@@ -57,14 +118,15 @@ class Md2ModelLoader(val locator: ResourceLocator) {
         )
         val size = vertexBuffer.size / 5 // 5 floats per vertex : fixme: not great
         meshBuilder.addMesh(vertexBuffer.toFloatArray(), (0..<size).map { it.toShort() }.toShortArray())
-        return modelBuilder.end()
+        val model = modelBuilder.end()
+        return model
     }
-
-    private fun readMd2Model(modelData: ByteArray): Md2Model {
-        val byteBuffer = ByteBuffer
-            .wrap(modelData)
-            .order(ByteOrder.LITTLE_ENDIAN)
-        return Md2Model(byteBuffer)
-    }
-
 }
+
+private fun readMd2Model(modelData: ByteArray): Md2Model {
+    val byteBuffer = ByteBuffer
+        .wrap(modelData)
+        .order(ByteOrder.LITTLE_ENDIAN)
+    return Md2Model(byteBuffer)
+}
+
