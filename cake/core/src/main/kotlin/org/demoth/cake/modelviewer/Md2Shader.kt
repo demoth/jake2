3a5713da6ebfd321bd99a841abc528ab64a78774
Daniil Bubnov <bubnov.d.e@gmail.com>
2025-06-15 21:54:27 +0200

    documentation for md2 shader

--- cake/core/src/main/kotlin/org/demoth/cake/modelviewer/Md2Shader.kt
+++ cake/core/src/main/kotlin/org/demoth/cake/modelviewer/Md2Shader.kt
@@ -3,9 +3,11 @@ package org.demoth.cake.modelviewer
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g3d.Attributes
 import com.badlogic.gdx.graphics.g3d.Renderable
+import com.badlogic.gdx.graphics.g3d.Shader
 import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute
 import com.badlogic.gdx.graphics.g3d.shaders.BaseShader
 import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader
+import com.badlogic.gdx.graphics.g3d.utils.DefaultShaderProvider
 import com.badlogic.gdx.graphics.glutils.ShaderProgram
 
 data class Md2CustomData(
@@ -15,6 +17,10 @@ data class Md2CustomData(
     val frames: Int
 )
 
+/**
+ * The custom texture attribute, required to register and bind the animation texture.
+ * Practically, it's just another type of texture attribute.
+ */
 class AnimationTextureAttribute(val texture: Texture): TextureAttribute(AnimationTexture, texture) {
     companion object {
         @property:JvmStatic val AnimationTextureAlias: String = "animationTexture"
@@ -26,12 +32,18 @@ class AnimationTextureAttribute(val texture: Texture): TextureAttribute(Animatio
     }
 }
 
+/**
+ * Shader for md2 vertex animations using a Vertex Animation Texture (or VAT) approach.
+ *
+ * This class doesn't have any custom logic apart from the registration of the required uniforms and attributes.
+ * The [DefaultShader] superclass is already flexible enough to handle the rest.
+ */
 class Md2Shader(
     renderable: Renderable,
     config: Config,
     shaderProgram: ShaderProgram,
 ): DefaultShader(renderable, config, shaderProgram) {
-    // uniform setters
+    // uniform setters (local as they are different for each object)
     private val vertexAnimationTextureSetter = object : LocalSetter() {
         override fun set(shader: BaseShader, inputID: Int, renderable: Renderable?, combinedAttributes: Attributes) {
             // identify which unit to bind to
@@ -39,13 +51,12 @@ class Md2Shader(
                 (combinedAttributes.get(AnimationTextureAttribute.AnimationTexture) as TextureAttribute).textureDescription
             val unit = shader.context.textureBinder.bind(textureDescription)
             shader.set(inputID, unit)
-            // also set the animation texture height and width
+
+            // since here we already know the size of the vat, we can also set its height and width uniforms
             shader.set(u_textureHeightPos, textureDescription.texture.height)
             shader.set(u_textureWidthPos, textureDescription.texture.width)
         }
     }
-
-    // local setters (unique values for each object)
     private val frame1Setter = object : LocalSetter() {
         override fun set(shader: BaseShader, inputID: Int, renderable: Renderable, combinedAttributes: Attributes) {
             val md2CustomData = renderable.userData as Md2CustomData
@@ -66,20 +77,30 @@ class Md2Shader(
     }
 
     // animation related local (per renderable) uniforms
-    private val u_vertexAnimationTexture = Uniform("u_vertexAnimationTexture")
+    protected val u_vertexAnimationTexture = Uniform("u_vertexAnimationTexture")
     private val u_vertexAnimationTextureHeight = Uniform("u_textureHeight")
     private val u_vertexAnimationTextureWidth = Uniform("u_textureWidth")
     private val u_frame1 = Uniform("u_frame1")
     private val u_frame2 = Uniform("u_frame2")
     private val u_interpolation = Uniform("u_interpolation")
 
-    // register additional uniforms
+    // register the uniforms
     private val u_vertexAnimationTexturePos = register(u_vertexAnimationTexture, vertexAnimationTextureSetter)
+    // no setter for these two as they are set inside the setter for u_vertexAnimationTexturePos
     private val u_textureHeightPos = register(u_vertexAnimationTextureHeight)
     private val u_textureWidthPos = register(u_vertexAnimationTextureWidth)
     private val u_frame1Pos = register(u_frame1, frame1Setter)
     private val u_frame2Pos = register(u_frame2, frame2Setter)
     private val u_interpolationPos = register(u_interpolation, interpolationSetter)
+}
 
-
+/**
+ * Shader provider for md2 animations, uses the user data to determine whether to use the [Md2Shader] or not.
+ */
+class Md2ShaderProvider(private val shader: Shader): DefaultShaderProvider() {
+    override fun getShader(renderable: Renderable): Shader? {
+        return if (renderable.userData is Md2CustomData) {
+            shader
+        } else super.getShader(renderable)
+    }
 }
