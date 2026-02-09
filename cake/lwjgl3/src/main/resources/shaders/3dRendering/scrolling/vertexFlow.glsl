72b30c89ed13a945a1a1fa0cf131c680e94913fe
Daniil Bubnov <bubnov.d.e@gmail.com>
2026-02-09 16:47:17 +0100

    chore: remove unused shaders

--- cake/lwjgl3/src/main/resources/shaders/3dRendering/scrolling/vertexFlow.glsl
+++
@@ -1,7 +0,0 @@
-// it gets attributes and uniforms from vertexCommon3D
-
-void main()
-{
-    passTexCoord = texCoord + vec2(scroll, 0.0);
-    gl_Position = transProjView * transModel * vec4(position, 1.0);
-}
--- cake/lwjgl3/src/main/resources/shaders/3dRendering/water/vertexWater.glsl
+++
@@ -1,7 +0,0 @@
-// it gets attributes and uniforms from vertexCommon3D
-void main()
-{
-    passTexCoord = texCoord;
-
-    gl_Position = transProjView * transModel * vec4(position, 1.0);
-}
